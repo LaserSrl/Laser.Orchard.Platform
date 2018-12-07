a3ef4920d479da0bf09ea9b44b6a64090ab9b54b
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2018-12-07 15:19:03 +0100

    - Give a point of extensibility in searching a localized Url, for not orchard contentents for example

---
+++ src/Modules/Laser.Orchard.CulturePicker/Services/ContentLocalizableRouteService.cs
@@ -0,0 +1,60 @@
+﻿using Orchard;
+using Orchard.Autoroute.Models;
+using Orchard.Environment.Configuration;
+using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.CulturePicker.Services {
+    public class ContentLocalizableRouteService : ILocalizableRouteService {
+        private readonly ILocalizableContentService _localizableContentService;
+
+        public ContentLocalizableRouteService(ILocalizableContentService localizableContentService,
+                                              IOrchardServices services) {
+            _localizableContentService = localizableContentService;
+            Services = services;
+
+        }
+
+        public int Priority {
+            get {
+                return 0;
+            }
+        }
+
+        public IOrchardServices Services { get; set; }
+        public Localizer T { get; set; }
+
+
+        public bool TryFindLocalizedUrl(LocalizableRouteContext localizableRouteContext) {
+            bool translatedUrlFound = false;
+
+            if (string.IsNullOrEmpty(localizableRouteContext.Culture)) {
+                throw new ArgumentNullException(localizableRouteContext.Culture);
+            }
+            var urlPrefix = Services.WorkContext.Resolve<ShellSettings>().RequestUrlPrefix;
+
+            string returnUrl = Utils.GetReturnUrl(Services.WorkContext.HttpContext.Request, urlPrefix);
+
+            AutoroutePart currentRoutePart;
+            //returnUrl may not correspond to any content and we use "Try" approach
+            if (_localizableContentService.TryGetRouteForUrl(returnUrl, out currentRoutePart)) {
+                AutoroutePart localizedRoutePart;
+                //content may not have localized version and we use "Try" approach
+                if (_localizableContentService.TryFindLocalizedRoute(currentRoutePart.ContentItem, localizableRouteContext.Culture, out localizedRoutePart)) {
+                    returnUrl = localizedRoutePart.PromoteToHomePage ? "" : localizedRoutePart.Path;
+                    if (!String.IsNullOrWhiteSpace(urlPrefix) && !returnUrl.StartsWith(urlPrefix)) {
+                        //returnUrl = urlPrefix + "/" + returnUrl;
+                        returnUrl = "~/" + returnUrl;
+                    }
+                    localizableRouteContext.UrlLocalized = returnUrl;
+                    translatedUrlFound = true;
+                }
+            }
+
+            return translatedUrlFound;
+        }
+    }
+}
---
+++ src/Modules/Laser.Orchard.CulturePicker/Services/LocalizableRouteContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.CulturePicker.Services {
+    public class LocalizableRouteContext {
+        public string QuerystringToLocalize { get; set; }
+        public string UrlToLocalize { get; set; }
+        public string Culture { get; set; }
+        public string UrlLocalized { get; set; }
+        public string QuerystringLocalized { get; set; }
+    }
+}
