febf27697a50e955ab5fd27bf0d3f553e766ecd2
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2019-01-22 11:05:13 +0100

    Updated filter provider to correctly work around the issue with nhibernate being unable to handle parameters that are present more than once in the query

--- src/Modules/Laser.Orchard.Queries/Providers/HQLQueryFilterProvider.cs
+++ src/Modules/Laser.Orchard.Queries/Providers/HQLQueryFilterProvider.cs
@@ -11,6 +11,7 @@ using Orchard.Localization;
 using Orchard.ContentManagement;
 using NHibernate;
 using Orchard.Data;
+using System.Text.RegularExpressions;
 
 namespace Laser.Orchard.Queries.Providers {
     [OrchardFeature("Laser.Orchard.HQLProjectionFilter")]
@@ -71,11 +72,37 @@ namespace Laser.Orchard.Queries.Providers {
             string subquery, 
             Dictionary<string, object> parameters) {
 
-            var hqlQuery = _session.CreateQuery(subquery);
+            // NHibernate fails to assign values to all instances of a same parameter.
+            // As a workaround, we create a new "copy" of the query, to handle multiplicity.
+            var qString = subquery;
+            // We use the base query to compute the substitutions in the actual query.
+            var baseQuery = _session.CreateQuery(subquery);
+            // Dictionary of the substitutions. Keys are the parameter names in the "new" query.
+            // Values are the "old" parameter names. 
+            var substitutionMap = new Dictionary<string, string>();
+            foreach (var para in baseQuery.NamedParameters) {
+                var pCode = $":{para}"; // Parameters in the query are prepended by :
+                var rgx = new Regex(pCode);
+                // while there is a match with the original parameter, replace it
+                for (Match match = rgx.Match(qString); match.Success; match = rgx.Match(qString)) {
+                    var newParam = $"param_{substitutionMap.Count}";
+                    qString = rgx.Replace(
+                        qString, 
+                        $":{newParam}",
+                        1,
+                        (match.Index - 1) < 0 ? 0 : (match.Index));
+                    // add the information on the substitution to the map
+                    substitutionMap.Add(newParam, para);
+                }
+            }
+            // Now create the actual query
+            var hqlQuery = _session.CreateQuery(qString);
             foreach (var para in hqlQuery.NamedParameters) {
+                // get the original parameter name
+                var oldPara = substitutionMap[para];
                 // OWASP says nhibernate's SetParameter is safe
-                if (parameters.ContainsKey(para)) {
-                    hqlQuery.SetParameter(para, parameters[para]);
+                if (parameters.ContainsKey(oldPara)) {
+                    hqlQuery.SetParameter(para, parameters[oldPara]);
                 } else {
                     hqlQuery.SetParameter(para, "");
                 }
