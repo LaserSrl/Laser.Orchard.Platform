be70ccc5863629db28b518157cb99e150308ce04
giuseppe.musso <giuseppe.musso@laser-group.com>
2019-03-05 16:18:28 +0100

    added a class to model badge infos

---
+++ src/Modules/Laser.Orchard.StartupConfig/AccessManagement/BadgeInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Laser.Orchard.StartupConfig.AccessManagement
+{
+    public class BadgeInfo
+    {
+        public string PartNumber { get; set; }
+        public string MobileId { get; set; }
+    }
+}
