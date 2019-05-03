d2aed4322f50f8993efa7b89d9bdc4c0eaf55efa
giuseppe.musso <giuseppe.musso@laser-group.com>
2019-05-03 16:31:31 +0200

    fixed ReactionApi when pageId is not a published content

--- src/Modules/Laser.Orchard.UserReactions/ViewModels/ReactionsSummaryVM.cs
+++ src/Modules/Laser.Orchard.UserReactions/ViewModels/ReactionsSummaryVM.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Runtime.Serialization;
 
 namespace Laser.Orchard.UserReactions.ViewModels {
+    [DataContract]
     public class ReactionsSummaryVM {
+        [DataMember(Name = "UserAuthenticated")]
         public bool UserAuthenticated { get; set; }
+        [DataMember(Name = "UserAuthorized")]
         public bool UserAuthorized { get; set; }
+        [DataMember(Name = "ContentId")]
         public int ContentId { get; set; }
+        [DataMember(Name = "Reactions")]
         public UserReactionsVM[] Reactions { get; set; }
 
         public ReactionsSummaryVM() {
