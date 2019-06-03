d31c971fe6c493a54ff49aa9e9ddef6288aabdd5
SimoneLazzaro-Laser <simone.lazzaro@laser-group.com>
2019-06-03 12:57:07 +0200

    add open route service site settings for saving api key

---
+++ src/Modules/Laser.Orchard.Maps/Drivers/OpenRouteSiteSettingsPartDriver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Laser.Orchard.Maps.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Localization;
+
+namespace Laser.Orchard.Maps.Drivers {
+    public class OpenRouteSiteSettingsPartDriver : ContentPartDriver<OpenRouteSiteSettingsPart> {
+        private const string TemplateName = "Parts/OpenRouteSiteSettings";
+
+        public OpenRouteSiteSettingsPartDriver() {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        protected override string Prefix { get { return "OpenRouteSettings"; } }
+
+        protected override DriverResult Editor(OpenRouteSiteSettingsPart part, dynamic shapeHelper) {
+            return Editor(part, null, shapeHelper);
+        }
+
+        protected override DriverResult Editor(OpenRouteSiteSettingsPart part, IUpdateModel updater, dynamic shapeHelper) {
+            return ContentShape("Parts_OpenRouteSiteSettings_Edit", () => {
+                if (updater != null) {
+                    updater.TryUpdateModel(part, Prefix, null, null);
+                }
+                return shapeHelper.EditorTemplate(TemplateName: TemplateName, Model: part, Prefix: Prefix);
+            })
+            .OnGroup("OpenRouteService");
+        }
+    }
+}
---
+++ src/Modules/Laser.Orchard.Maps/Handlers/OpenRouteSiteSettingsHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Laser.Orchard.Maps.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+using Orchard.Logging;
+
+namespace Laser.Orchard.Maps.Handlers {
+    public class OpenRouteSiteSettingsHandler : ContentHandler {
+
+        public OpenRouteSiteSettingsHandler() {
+            T = NullLocalizer.Instance;
+            Logger = NullLogger.Instance;
+
+            Filters.Add(new ActivatingFilter<OpenRouteSiteSettingsPart>("Site"));
+        }
+
+        public Localizer T { get; set; }
+        protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            if (context.ContentItem.ContentType != "Site")
+                return;
+            base.GetItemMetadata(context);
+            context.Metadata.EditorGroupInfo.Add(new GroupInfo(T("OpenRouteService")));
+        }
+
+    }
+}
