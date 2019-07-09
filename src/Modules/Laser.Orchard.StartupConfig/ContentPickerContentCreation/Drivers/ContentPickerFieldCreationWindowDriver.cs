6a3b947f32e5ae5e4381e9f479c0727692dd4a87
marco.viglione <marco.viglione@laser-group.com>
2019-07-09 16:26:42 +0200

    Meccanismo per nascondere tema admin in finestre di creazione Content Picker

---
+++ src/Modules/Laser.Orchard.StartupConfig/ContentPickerContentCreation/Drivers/ContentPickerFieldCreationWindowDriver.cs
@@ -0,0 +1,44 @@
+﻿using Orchard;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Core.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Laser.Orchard.StartupConfig.ContentPickerContentCreation.Drivers {
+    public class ContentPickerFieldCreationWindowDriver : ContentPartDriver<CommonPart> {
+
+        private readonly IOrchardServices _orchardServices;
+
+        public ContentPickerFieldCreationWindowDriver(IOrchardServices orchardServices) {
+            _orchardServices = orchardServices;
+        }
+
+        protected override string Prefix {
+            get { return "Laser.Orchard.ContentPickerContentCreation"; }
+        }
+
+        protected override DriverResult Editor(CommonPart part, dynamic shapeHelper) {
+
+            var request = _orchardServices.WorkContext.HttpContext.Request;
+
+            var routeData = request.RequestContext.RouteData;
+            var isContentPickerCreation = request.QueryString["callback"] != null && request.QueryString["callback"].StartsWith("_contentpickercreate_");
+
+            if (((Route)routeData.Route).Url.StartsWith("Admin/Contents/{action}") && routeData.Values["action"].Equals("Create") && isContentPickerCreation) {
+                return ContentShape("Parts_ContentPickerCreateItem_EditExtension", () => shapeHelper.EditorTemplate(TemplateName: "Parts/ContentPickerCreateItem.EditExtension",
+                                                                                                       Model: null,
+                                                                                                       Prefix: Prefix));
+            }
+
+            return null;
+        }
+
+        protected override DriverResult Editor(CommonPart part, IUpdateModel updater, dynamic shapeHelper) {
+            return Editor(part, shapeHelper);
+        }
+    }
+}
---
+++ src/Modules/Laser.Orchard.StartupConfig/ContentPickerContentCreation/Filters/CPDisableThemeFilter.cs
@@ -0,0 +1,26 @@
+﻿using Orchard.Mvc.Filters;
+using Orchard.Themes;
+using Orchard.UI.Resources;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.StartupConfig.ContentPickerContentCreation.Filters {
+    public class CPDisableThemeFilter : FilterProvider, IActionFilter {
+        private readonly IResourceManager _resourceManager;
+
+        public CPDisableThemeFilter(IResourceManager resourceManager) {
+            _resourceManager = resourceManager;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext) {
+            if (filterContext.ActionDescriptor.ActionName == "Create" && filterContext.ActionDescriptor.ControllerDescriptor.ControllerName == "Admin") {
+                //ThemeFilter.Disable(filterContext.RequestContext);
+
+                var cssPath = "/Themes/TheAdmin/Styles/test.css";
+                _resourceManager.Include("stylesheet", cssPath, cssPath).AtFoot();
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext) {
+        }
+    }
+}
