21187c32b42e4ebbf1d2ad0ead6bf495d3080ee6
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2019-09-10 12:53:01 +0200

    Fixed a bug: ContentLocalizableRouteService would report that a page were localizable even when it was not a ContentItem, because it would default to the home page.

--- src/Modules/Laser.Orchard.CulturePicker/Services/Utils.cs
+++ src/Modules/Laser.Orchard.CulturePicker/Services/Utils.cs
@@ -22,6 +22,18 @@ namespace Laser.Orchard.CulturePicker.Services {
             return HttpUtility.UrlDecode(localUrl);
         }
 
+        public static string GetCleanUrl(HttpRequestBase request, string path, string prefix) {
+            string localUrl = GetAppRelativePath(path, request);
+            if (!String.IsNullOrWhiteSpace(prefix)) {
+                localUrl = localUrl.StartsWith(prefix, StringComparison.OrdinalIgnoreCase) 
+                    ? localUrl.Substring(prefix.Length) : localUrl;
+            }
+            localUrl = localUrl.StartsWith("/") ? localUrl.Substring(1) : localUrl;
+            localUrl = HttpUtility.UrlDecode(localUrl);
+            //support for pre-encoded Unicode urls
+            return HttpUtility.UrlDecode(localUrl);
+        }
+
         //Translates an ASP.NET path like /myapp/subdir/page.aspx 
         //into an application relative path: subdir/page.aspx. The
         //path returned is based of the application base and 
