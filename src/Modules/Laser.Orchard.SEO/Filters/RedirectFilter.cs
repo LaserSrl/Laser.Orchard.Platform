7e20df1f54ab5c878e1acc567f5f1b55d104fc9e
giuseppe.musso <giuseppe.musso@laser-group.com>
2019-09-13 11:51:42 +0200

    fixed SEO redirect path

--- src/Modules/Laser.Orchard.SEO/Filters/RedirectFilter.cs
+++ src/Modules/Laser.Orchard.SEO/Filters/RedirectFilter.cs
@@ -56,7 +56,7 @@ namespace Laser.Orchard.SEO.Filters {
                         .FirstOrDefault(s => s.Equals(urlPrefix.Trim('/'), StringComparison.InvariantCultureIgnoreCase)));
             }
             strippedSegments.RemoveAll(s => string.IsNullOrEmpty(s));
-            var basePath = url.AbsoluteUri.Substring(0, url.AbsoluteUri.IndexOf(applicationPath));
+            
             //if querystring is in redirects table, use it
             var pathQs = string.Join("/", strippedSegments) + url.Query;
             var redirect = _redirectService.GetRedirect(pathQs);
@@ -65,14 +65,14 @@ namespace Laser.Orchard.SEO.Filters {
                 var path = string.Join("/", strippedSegments);
                 redirect = _redirectService.GetRedirect(path);
                 if (redirect != null) {
-                    var destination = basePath + applicationPath +
+                    var destination = _wca.GetContext().CurrentSite.BaseUrl + //not a fan of this, since BaseUrl can be edited by admin
                         (string.IsNullOrWhiteSpace(urlPrefix) ? "" : "/" + urlPrefix) +
                         "/" + redirect.DestinationUrl.TrimStart('/');
                     filterContext.Result = new RedirectResult(destination + url.Query, redirect.IsPermanent);
                 }
             }
             else {
-                var destination = basePath + applicationPath +
+                var destination = _wca.GetContext().CurrentSite.BaseUrl + //not a fan of this, since BaseUrl can be edited by admin
                     (string.IsNullOrWhiteSpace(urlPrefix) ? "" : "/" + urlPrefix) +
                     "/" + redirect.DestinationUrl.TrimStart('/');
                 filterContext.Result = new RedirectResult(destination, redirect.IsPermanent);
