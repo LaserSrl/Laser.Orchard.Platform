fb3faeadfb766cb15cf40d56b31d0a9a37efc8dd
giuseppe.musso <giuseppe.musso@laser-group.com>
2019-10-02 16:23:11 +0200

    timymce enhancement: wip

---
+++ src/Modules/Laser.Orchard.StartupConfig/TinyMceEnhancement/TinyMceShapeDisplayEvent.cs
@@ -0,0 +1,22 @@
+﻿using Orchard.DisplayManagement.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.StartupConfig.TinyMceEnhancement {
+    public class TinyMceShapeDisplayEvent : ShapeDisplayEvents {
+        public override void Displaying(ShapeDisplayingContext context) {
+            if (String.CompareOrdinal(context.ShapeMetadata.Type, "Body_Editor") != 0) {
+                return;
+            }
+
+            if (!String.Equals(context.Shape.EditorFlavor, "html", StringComparison.InvariantCultureIgnoreCase)) {
+                return;
+            }
+
+            var aux = context;
+        }
+
+    }
+}
