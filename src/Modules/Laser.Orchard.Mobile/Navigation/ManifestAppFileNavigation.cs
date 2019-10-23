76cf8626a56a52a73ad5757f72ccfea2035e0a80
diego.marguerettaz <diego.marguerettaz@laser-group.com>
2019-10-23 14:34:04 +0200

    Tolto il "commento" nella Navigation/ManifestAppFileNavigation.cs

--- src/Modules/Laser.Orchard.Mobile/Navigation/ManifestAppFileNavigation.cs
+++ src/Modules/Laser.Orchard.Mobile/Navigation/ManifestAppFileNavigation.cs
@@ -1,24 +1,24 @@
-﻿//using Orchard.Localization;
-//using Orchard.UI.Navigation;
-//using Laser.Orchard.Mobile.PermissionsManifestAppFile;
+﻿using Orchard.Localization;
+using Orchard.UI.Navigation;
+using Laser.Orchard.Mobile.PermissionsManifestAppFile;
 
-//namespace Laser.Orchard.Mobile.Navigation {
-//    public class ManifestAppFileNavigation : INavigationProvider {
-//        public Localizer T { get; set; }
-//        public string MenuName { get { return "admin"; } }
+namespace Laser.Orchard.Mobile.Navigation {
+    public class ManifestAppFileNavigation : INavigationProvider {
+        public Localizer T { get; set; }
+        public string MenuName { get { return "admin"; } }
 
-//        public ManifestAppFileNavigation() {
-//            T = NullLocalizer.Instance;
-//        }
+        public ManifestAppFileNavigation() {
+            T = NullLocalizer.Instance;
+        }
 
-//        public void GetNavigation(NavigationBuilder builder) {
-//            builder.Add(T("Settings"),  menu => menu.LinkToFirstChild(false).Permission(ManifestAppFilePermissions.ManifestAppFile) 
-//                    .Add(item => item
-//                        .Caption(T("Manifest App File"))
-//                        .Position("10")
-//                        .Action("Index", "ManifestAppFileAdmin", new { area = "Laser.Orchard.Mobile" })
-//                        .Permission(ManifestAppFilePermissions.ManifestAppFile)
-//                    ));
-//        }
-//    }
-//}
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.Add(T("Settings"), menu => menu.LinkToFirstChild(false).Permission(ManifestAppFilePermissions.ManifestAppFile)
+                   .Add(item => item
+                       .Caption(T("Manifest App File"))
+                       .Position("10")
+                       .Action("Index", "ManifestAppFileAdmin", new { area = "Laser.Orchard.Mobile" })
+                       .Permission(ManifestAppFilePermissions.ManifestAppFile)
+                   ));
+        }
+    }
+}
