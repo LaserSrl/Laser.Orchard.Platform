cc7540db1ecf174e926a01edc9cc35b2f17c3d3e
GiuseppeMusso-Laser <giuseppe.musso@laser-group.com>
2019-10-28 10:38:44 +0100

    temporary fix for tinymce enhancement

---
+++ src/Modules/Laser.Orchard.StartupConfig/TinyMceEnhancement/ResourceManifest.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.Environment.Extensions;
+using Orchard.UI.Resources;
+
+namespace Laser.Orchard.StartupConfig.TinyMceEnhancement {
+    [OrchardFeature("Laser.Orchard.StartupConfig.TinyMceEnhancement")]
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            // tinymce enhancement: replace standard orchard init with custom init
+            manifest.DefineScript("OrchardTinyMce").SetUrl("laser-tinymce.js").SetDependencies("TinyMce");
+        }
+    }
+}
