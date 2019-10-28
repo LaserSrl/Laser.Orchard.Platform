b2ac4047da476430264387a4e27ddc08c89e3473
GiuseppeMusso-Laser <giuseppe.musso@laser-group.com>
2019-10-28 12:44:24 +0100

    fixed tinymce enhancement require of OrchardTinyMce js

--- src/Modules/Laser.Orchard.StartupConfig/TinyMceEnhancement/ResourceManifest.cs
+++ src/Modules/Laser.Orchard.StartupConfig/TinyMceEnhancement/ResourceManifest.cs
@@ -8,7 +8,7 @@ namespace Laser.Orchard.StartupConfig.TinyMceEnhancement {
             var manifest = builder.Add();
 
             // tinymce enhancement: replace standard orchard init with custom init
-            manifest.DefineScript("OrchardTinyMce").SetUrl("laser-tinymce.js").SetDependencies("TinyMce");
+            manifest.DefineScript("OrchardTinyMce").SetUrl("laser-tinymce.js").SetDependencies("TinyMce").SetVersion("1.1");
         }
     }
 }
