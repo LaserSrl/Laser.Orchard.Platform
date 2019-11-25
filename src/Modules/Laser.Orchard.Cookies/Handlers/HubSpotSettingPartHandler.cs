a8f28a41c51b046be7cded2b648104222ed9a276
elena.lampugnani <elena.lampugnani@laser-group.com>
2019-11-25 17:00:57 +0100

    add first part of setting still missing some details and reading the value

---
+++ src/Modules/Laser.Orchard.Cookies/Handlers/HubSpotSettingPartHandler.cs
@@ -0,0 +1,31 @@
+﻿using Laser.Orchard.Cookies.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+using Orchard.Localization;
+using Orchard.Logging;
+
+namespace Laser.Orchard.Cookies.Handlers {
+    public class HubSpotSettingPartHandler : ContentHandler {
+        public HubSpotSettingPartHandler()
+        {
+            T = NullLocalizer.Instance;
+            Logger = NullLogger.Instance;
+            Filters.Add(new ActivatingFilter<HubSpotSettingsPart>("Site"));
+        }
+
+        public Localizer T { get; set; }
+        protected override void GetItemMetadata(GetContentItemMetadataContext context)
+        {
+            if (context.ContentItem.ContentType != "Site")
+            {
+                return;
+            }
+            base.GetItemMetadata(context);
+            var groupInfo = new GroupInfo(T("HubSpot"));
+            groupInfo.Id = "HubSpot";
+            context.Metadata.EditorGroupInfo.Add(groupInfo);
+        }
+
+    }
+}
