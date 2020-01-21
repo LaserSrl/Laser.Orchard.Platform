3a8d7a4a889bb21e761d7a9e29c2661612d202a4
elena.lampugnani <elena.lampugnani@laser-group.com>
2020-01-21 11:01:06 +0100

    fix driver

--- src/Modules/Laser.Orchard.NwazetIntegration/Drivers/OrderPartDriver.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Drivers/OrderPartDriver.cs
@@ -11,16 +11,13 @@ namespace Laser.Orchard.NwazetIntegration.Drivers {
     public class OrderPartDriver : ContentPartDriver<OrderPart> {
 
         private readonly IOrchardServices _orchardServices;
-        private readonly IRepository<PaymentRecord> _repository;
         private readonly IPaymentService _paymentService;
 
         public OrderPartDriver(
             IOrchardServices orchardServices,
-            IRepository<PaymentRecord> repository,
             IPaymentService paymentService)
         {
             _orchardServices = orchardServices;
-            _repository = repository;
             _paymentService = paymentService;
 
             T = NullLocalizer.Instance;
@@ -28,35 +25,27 @@ namespace Laser.Orchard.NwazetIntegration.Drivers {
 
         public Localizer T { get; set; }
 
-        protected override DriverResult Display(
-            OrderPart part, string displayType, dynamic shapeHelper)
-        {
-            return null;
-        }
-
         //GET
         protected override DriverResult Editor(OrderPart order, dynamic shapeHelper)
         {
             if (!_orchardServices.Authorizer.Authorize(OrderPermissions.ManageOrders, null, T("Cannot manage orders")))
                 return null;
 
-            var payment = _paymentService.GetPaymentByGuid(order.Charge.TransactionId);
+            PaymentRecord payment = _paymentService.GetPaymentByGuid(order.Charge.TransactionId);
 
             if(payment != null) {
-                PaymentRecord result = _repository.Get(payment.Id);
-
                 return ContentShape("Parts_Order_PaymentInfo",
                     () => {
                         var model = new PaymentInfoViewModel
                         {
-                            PosName = result.PosName,
-                            Reason = result.Reason,
-                            Amount = result.Amount,
-                            Currency = result.Currency,
-                            UpdateDate= result.UpdateDate,
-                            Success = result.Success,
-                            Error = result.Error,
-                            TransationId = result.TransactionId,
+                            PosName = payment.PosName,
+                            Reason = payment.Reason,
+                            Amount = payment.Amount,
+                            Currency = payment.Currency,
+                            UpdateDate= payment.UpdateDate,
+                            Success = payment.Success,
+                            Error = payment.Error,
+                            TransationId = payment.TransactionId,
                         };
                         return shapeHelper.EditorTemplate(
                             TemplateName: "Parts/Order.PaymentInfo",
