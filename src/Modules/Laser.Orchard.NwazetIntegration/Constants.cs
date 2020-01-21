7c54c973614d64d5e9d0d52b04640ef474af0e11
elena.lampugnani <elena.lampugnani@laser-group.com>
2020-01-21 15:06:45 +0100

    manage status order

---
+++ src/Modules/Laser.Orchard.NwazetIntegration/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.NwazetIntegration {
+    public class Constants {
+        public const string PaymentSucceeded = "PaymentSucceeded";
+        public const string PaymentFailed= "Payment Failed";
+    }
+}
