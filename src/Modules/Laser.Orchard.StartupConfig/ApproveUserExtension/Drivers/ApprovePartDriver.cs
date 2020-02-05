e817bcb92c620ff90bd39b469140b5fe623130f4
elena.lampugnani <elena.lampugnani@laser-group.com>
2020-02-05 16:14:46 +0100

    manage approve user in your edit shape

--- src/Modules/Laser.Orchard.StartupConfig/ApproveUserExtension/Drivers/ApprovePartDriver.cs
+++ src/Modules/Laser.Orchard.StartupConfig/ApproveUserExtension/Drivers/ApprovePartDriver.cs
@@ -1,36 +1,45 @@
-﻿using Orchard;
+﻿using Laser.Orchard.StartupConfig.ApproveUserExtension.Services;
+using Orchard;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.Localization;
+using Orchard.Mvc;
 using Orchard.Users.Models;
 using Orchard.Users.ViewModels;
+using System;
 
 namespace Laser.Orchard.StartupConfig.ApproveUserExtension.Drivers {
     public class ApprovePartDriver : ContentPartDriver<UserPart> {
         private const string TemplateName = "Parts/UserApprove";
-        private readonly IOrchardServices _orchardServices;
+        private readonly IApproveUserService _approveUserService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public ApprovePartDriver(
-            IOrchardServices orchardServices
+            IApproveUserService approveUserService,
+            IHttpContextAccessor httpContextAccessor
             ) {
-            _orchardServices = orchardServices;
+
+            _approveUserService = approveUserService;
+            _httpContextAccessor = httpContextAccessor;
+
+            T = NullLocalizer.Instance;
         }
 
-        private UserEditViewModel BuildViewModelFromPart(int id) {
-            var user = _orchardServices.ContentManager.Get(id).As<UserPart>();
-
-            if (user == null)
-                return new UserEditViewModel();
-
-            return new UserEditViewModel {
-                User = user
-            };
-        }
+        public Localizer T { get; set; }
 
         protected override DriverResult Editor(UserPart part, dynamic shapeHelper) {
-            var model = BuildViewModelFromPart(part.Id);
+            var model = new UserEditViewModel { User = part };
 
             return ContentShape("Parts_UserApprove_Edit",
                                 () => shapeHelper.EditorTemplate(TemplateName: TemplateName, Model: model, Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(UserPart part, IUpdateModel updater, dynamic shapeHelper) {
+            var model = new UserEditViewModel { User = part };
+
+            return ContentShape("Parts_UserApprove_Edit",
+                              () => shapeHelper.EditorTemplate(TemplateName: TemplateName, Model: model, Prefix: Prefix));
+
         }
     }
 }
---
+++ src/Modules/Laser.Orchard.StartupConfig/ApproveUserExtension/Services/ApproveUserService.cs
@@ -0,0 +1,62 @@
+﻿using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Localization;
+using Orchard.Users;
+using Orchard.Security;
+using Orchard.Users.Models;
+using Orchard.UI.Notify;
+using Orchard.Users.Events;
+
+namespace Laser.Orchard.StartupConfig.ApproveUserExtension.Services {
+    public interface IApproveUserService : IDependency {
+        void Approve(ContentItem contentItem);
+        void Disable(ContentItem contentItem);
+    }
+
+    public class ApproveUserService : IApproveUserService {
+
+        private readonly IUserEventHandler _userEventHandlers;
+        public IOrchardServices Services { get; set; }
+
+
+        public ApproveUserService(
+           IUserEventHandler userEventHandlers,
+           IOrchardServices services) {
+
+            _userEventHandlers = userEventHandlers;
+            Services = services;
+
+            T = NullLocalizer.Instance;
+        }
+        public Localizer T { get; set; }
+
+
+        public void Approve(ContentItem contentItem) {
+            if (!Services.Authorizer.Authorize(Permissions.ManageUsers, T("Not authorized to manage users")))
+                return;
+
+            var user = Services.ContentManager.Get<IUser>(contentItem.Id);
+
+            if (user == null)
+                return;
+
+            user.As<UserPart>().RegistrationStatus = UserStatus.Approved;
+            Services.Notifier.Information(T("User {0} approved", user.UserName));
+            _userEventHandlers.Approved(user);
+        }
+
+        public void Disable(ContentItem contentItem) {
+            if (!Services.Authorizer.Authorize(Permissions.ManageUsers, T("Not authorized to manage users")))
+                return;
+
+            var user = Services.ContentManager.Get<IUser>(contentItem.Id);
+
+            if (user == null)
+                return;
+
+            user.As<UserPart>().RegistrationStatus = UserStatus.Pending;
+            Services.Notifier.Information(T("User {0} disabled", user.UserName));
+            _userEventHandlers.Moderate(user);
+        }
+    }
+}
