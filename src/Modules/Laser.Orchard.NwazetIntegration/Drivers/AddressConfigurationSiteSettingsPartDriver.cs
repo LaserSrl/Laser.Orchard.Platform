c0c64c7a8fbb9221ea1f8ee03912ee1bd290ccde
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-02-20 17:45:08 +0100

    Export for address settigns

--- src/Modules/Laser.Orchard.NwazetIntegration/Drivers/AddressConfigurationSiteSettingsPartDriver.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Drivers/AddressConfigurationSiteSettingsPartDriver.cs
@@ -1,5 +1,6 @@
 ﻿using Laser.Orchard.NwazetIntegration.Controllers;
 using Laser.Orchard.NwazetIntegration.Models;
+using Laser.Orchard.NwazetIntegration.Services;
 using Laser.Orchard.NwazetIntegration.ViewModels;
 using Newtonsoft.Json;
 using Nwazet.Commerce.Controllers;
@@ -7,23 +8,28 @@ using Nwazet.Commerce.Models;
 using Nwazet.Commerce.ViewModels;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
 using Orchard.Environment.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Laser.Orchard.NwazetIntegration.Drivers {
     public class AddressConfigurationSiteSettingsPartDriver
         : ContentPartDriver<AddressConfigurationSiteSettingsPart> {
 
         private readonly IContentManager _contentManager;
+        private readonly IAddressConfigurationService _addressConfigurationService;
 
         public AddressConfigurationSiteSettingsPartDriver(
-            IContentManager contentManager) {
+            IContentManager contentManager,
+            IAddressConfigurationService addressConfigurationService) {
 
             _contentManager = contentManager;
+            _addressConfigurationService = addressConfigurationService;
         }
 
         protected override string Prefix {
@@ -141,6 +147,94 @@ namespace Laser.Orchard.NwazetIntegration.Drivers {
             };
 
             return vm;
+        }
+
+        protected override void Exporting(
+            AddressConfigurationSiteSettingsPart part, ExportContentContext context) {
+            var root = context.Element(part.PartDefinition.Name);
+            // base configuration
+            var hierarchy = _contentManager
+                .Get<TerritoryHierarchyPart>(part.ShippingCountriesHierarchyId);
+            var hierarchyIdentity = _contentManager
+                .GetItemMetadata(hierarchy).Identity;
+            root.SetAttributeValue("ShippingCountriesHierarchyId", hierarchyIdentity);
+            // details configuration
+            AddTerritoryElements(root, new TerritoryExportContext {
+                Ids = part.SelectedCountries,
+                ArrayName = "SelectedCountries",
+                ElementName = "SelectedCountry"
+            });
+            AddTerritoryElements(root, new TerritoryExportContext {
+                Ids = part.SelectedProvinces,
+                ArrayName = "SelectedProvinces",
+                ElementName = "SelectedProvince"
+            });
+            AddTerritoryElements(root, new TerritoryExportContext {
+                Ids = part.SelectedCities,
+                ArrayName = "SelectedCities",
+                ElementName = "SelectedCity"
+            });
+            // Country codes
+            root.AddEl(new XElement("CountryCodes")
+                .AddEl(part.CountryCodes
+                    .Select(cc => {
+                        var ele = new XElement("CountryCode");
+                        ele.SetAttributeValue("TerritoryName",
+                            _addressConfigurationService
+                                .SingleTerritory(cc.TerritoryId)
+                                ?.Record.TerritoryInternalRecord.Name ?? string.Empty);
+                        ele.SetAttributeValue("ISOCode",
+                            cc.ISOCode);
+                        return ele;
+                    })
+                    .ToArray()));
+        }
+
+        private void AddTerritoryElements(XElement root, TerritoryExportContext context) {
+            // our int[] should be exported as strings for the names of the
+            // TerritoryInternalRecords
+            var tNames = context.Ids
+                .Select(tirid => _addressConfigurationService
+                    .SingleTerritory(tirid)
+                    ?.Record.TerritoryInternalRecord.Name ?? string.Empty)
+                .Where(cn => !string.IsNullOrWhiteSpace(cn));
+            // for these arrays that represent TerritoryInternalRecord[], we
+            // do an xml element, with a child element for each territory.
+            // The reason is that in exporting/importing TerritoryInternalRecords
+            // we use their names rather than their ids, and it's non-trivial to
+            // be able to split them properly in an attribute.
+            root
+                .AddEl(new XElement(context.ArrayName)
+                    .AddEl(
+                        tNames
+                            .Select(cn => {
+                                var ele = new XElement(context.ElementName);
+                                ele.SetAttributeValue("Name", cn);
+                                return ele;
+                            })
+                            .ToArray()));
+        }
+        class TerritoryExportContext {
+            public int[] Ids { get; set; }
+            public string ArrayName { get; set; }
+            public string ElementName { get; set; }
+        }
+        protected override void Importing(
+            AddressConfigurationSiteSettingsPart part, ImportContentContext context) {
+            var root = context.Data.Element(part.PartDefinition.Name);
+            // Don't do anything if the tag is not specified.
+            if (root == null) {
+                return;
+            }
+
+            // base configuration
+            context.ImportAttribute(
+                part.PartDefinition.Name,
+                "ShippingCountriesHierarchyId",
+                identity =>
+                    part.ShippingCountriesHierarchyId = context.GetItemFromSession(identity).Id);
+            // details configuration
+            
         }
     }
 }
