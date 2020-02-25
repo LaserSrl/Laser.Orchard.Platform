da919e4295e61fcc38ba9f578eee99fb3e939435
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-02-25 17:30:04 +0100

    Changed UI for address configuration to progressively load children of displayed territories

--- src/Modules/Laser.Orchard.NwazetIntegration/Controllers/AddressConfigurationAdminController.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Controllers/AddressConfigurationAdminController.cs
@@ -1,4 +1,6 @@
-﻿using Nwazet.Commerce.Permissions;
+﻿using Laser.Orchard.NwazetIntegration.Services;
+using Nwazet.Commerce.Models;
+using Nwazet.Commerce.Permissions;
 using Orchard.ContentManagement;
 using Orchard.Data;
 using Orchard.Localization;
@@ -20,6 +22,8 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
         private readonly IContentManager _contentManager;
         private readonly ITransactionManager _transactionManager;
         private readonly INotifier _notifier;
+        private readonly IAddressConfigurationService _addressConfigurationService;
+        private readonly IAddressConfigurationSettingsService _addressSettingsService;
 
         private const string groupInfoId = "AddressConfigurationSiteSettings";
         
@@ -28,13 +32,17 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
             ISiteService siteService,
             IContentManager contentManager,
             ITransactionManager transactionManager,
-            INotifier notifier) {
+            INotifier notifier,
+            IAddressConfigurationService addressConfigurationService,
+            IAddressConfigurationSettingsService addressSettingsService) {
 
             _authorizer = authorizer;
             _siteService = siteService;
             _contentManager = contentManager;
             _transactionManager = transactionManager;
             _notifier = notifier;
+            _addressConfigurationService = addressConfigurationService;
+            _addressSettingsService = addressSettingsService;
 
             T = NullLocalizer.Instance;
         }
@@ -85,6 +93,52 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
 
         void IUpdateModel.AddModelError(string key, LocalizedString errorMessage) {
             ModelState.AddModelError(key, errorMessage.ToString());
+        }
+
+        [HttpPost]
+        public JsonResult GetChildren(int territoryId = 0) {
+            var parent = _addressConfigurationService
+                .SingleTerritory(territoryId);
+            if (parent == null) {
+                // this is an error
+                return null;
+            } else {
+                return Json(new {
+                    Success = true,
+                    Territories = parent.Children
+                        .Select(ci => {
+                            var tp = ci.As<TerritoryPart>();
+                            var id = tp.Record.TerritoryInternalRecord.Id;
+                            bool isCountry = false, isProvince = false, isCity = false, isNone = false;
+                            if (_addressSettingsService.SelectedCountryIds
+                                    .Contains(id)) {
+                                isCountry = true;
+                            } else if (_addressSettingsService.SelectedProvinceIds
+                                    .Contains(id)) {
+                                isProvince = true;
+                            } else if (_addressSettingsService.SelectedCityIds
+                                    .Contains(id)) {
+                                isCity = true;
+                            }else {
+                                isNone = true;
+                            }
+                            return new {
+                                Id = id,
+                                DisplayText = _contentManager
+                                    .GetItemMetadata(ci).DisplayText,
+                                IsCountry = isCountry,
+                                IsProvince = isProvince,
+                                IsCity = isCity,
+                                IsNone = isNone,
+                                CountryISO = _addressSettingsService.GetCountryISOCode(id),
+                                HasChildren = tp.Record.Children.Any(),
+                                ChildrenCount = tp.Record.Children.Count()
+                            };
+                        })
+                });
+            }
+            // TODO
+            return Json(new List<string>());
         }
     }
 }
