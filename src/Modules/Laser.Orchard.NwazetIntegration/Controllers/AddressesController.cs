00c83d7fc603884b5d77bb13a33f1cd822d48e65
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-03-05 15:49:33 +0100

    Added basic address validation in the checkout "flow", before creating the order, and in the address frontend CRUD

--- src/Modules/Laser.Orchard.NwazetIntegration/Controllers/AddressesController.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Controllers/AddressesController.cs
@@ -81,6 +81,11 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
                     result = RedirectToAction("Index", "ShoppingCart", new { area = "Nwazet.Commerce" });
                     break;
                 case "save":
+                    // validate addresses
+                    if (!ValidateVM(model.ShippingAddressVM) || !ValidateVM(model.BillingAddressVM)) {
+                        result = View("Index", model);
+                        break;
+                    }
                     // Hack: based on the address coming in model.ShippingAddressVM, we can compute the actual
                     // destinations to be used for tax computations at this stage
                     var countryName = _addressConfigurationService
@@ -226,7 +231,7 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
                 // advanced address stuff
                 // The string values here are the DisplayText properties of
                 // configured territories, or "custom" text entered by the user.
-                Country = vm.Country, 
+                Country = vm.Country,
                 City = vm.City,
                 Province = vm.Province
             };
@@ -293,6 +298,11 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
                 newAddress.Errors.Add(T("It was impossible to validate your address.").Text);
                 return View(newAddress);
             }
+            if (!ValidateVM(newAddress)) {
+                _transactionManager.Cancel();
+                newAddress.Errors.Add(T("It was impossible to validate your address.").Text);
+                return View(newAddress);
+            }
             // Convert the values of Country, City, and Province to strings and ids for
             // the AddressRecord.
             FixUpdate(newAddress);
@@ -337,6 +347,12 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
                 newAddress.Errors.Add(T("It was impossible to validate your address.").Text);
                 return View(newAddress);
             }
+
+            if (!ValidateVM(newAddress)) {
+                _transactionManager.Cancel();
+                newAddress.Errors.Add(T("It was impossible to validate your address.").Text);
+                return View(newAddress);
+            }
             // Convert the values of Country, City, and Province to strings and ids for
             // the AddressRecord.
             FixUpdate(newAddress);
@@ -357,7 +373,7 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
                 // this is an error
             } else {
                 var cities = _addressConfigurationService.GetAllCities(
-                    viewModel.IsBillingAddress 
+                    viewModel.IsBillingAddress
                         ? AddressRecordType.BillingAddress
                         : AddressRecordType.ShippingAddress,
                     country);
@@ -389,7 +405,7 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
                 var provinces = _addressConfigurationService.GetAllProvinces(
                     viewModel.IsBillingAddress
                         ? AddressRecordType.BillingAddress
-                        : AddressRecordType.ShippingAddress, 
+                        : AddressRecordType.ShippingAddress,
                     country, city);
                 return Json(new {
                     Success = true,
@@ -453,6 +469,51 @@ namespace Laser.Orchard.NwazetIntegration.Controllers {
                 BillingCountries = _addressConfigurationService.CountryOptions(AddressRecordType.BillingAddress),
                 CountryId = countryId
             };
+        }
+
+        /// <summary>
+        /// validation of the vm coming from a create/edit action
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// It would be cleaner to do this in its own validation classes,
+        /// but we need a bunch of IDependencies, so putting this code
+        /// here is less of an hassle.
+        /// </remarks>
+        private bool ValidateVM(AddressEditViewModel vm) {
+            var validCountries = _addressConfigurationService
+                .GetAllCountries(vm.AddressType);
+            var countryTP = _addressConfigurationService
+                .GetCountry(vm.CountryId);
+            if (!SubValidation(validCountries, countryTP)) {
+                return false;
+            }
+            var validProvinces = _addressConfigurationService
+                .GetAllProvinces(vm.AddressType, countryTP);
+            var provinceTP = GetTerritory(vm.Province);
+            if (!SubValidation(validProvinces, provinceTP)) {
+                return false;
+            }
+            var validCities = _addressConfigurationService
+                .GetAllCities(vm.AddressType, provinceTP);
+            var cityTP = GetTerritory(vm.City);
+            if (!SubValidation(validCities, cityTP)) {
+                return false;
+            }
+            return true;
+        }
+        private bool SubValidation(IEnumerable<TerritoryPart> list, TerritoryPart item) {
+            if (list == null || !list.Any()) {
+                return false;
+            }
+            if (item == null) {
+                return false;
+            }
+            if (!list.Any(c => c.Id == item.Id)) {
+                return false;
+            }
+            return true;
         }
 
         /// <summary>
