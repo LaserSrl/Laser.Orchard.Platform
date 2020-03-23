5a71131ac7b3288f9ce9ccf879e5d8b458a2c3e6
elena.lampugnani <elena.lampugnani@laser-group.com>
2020-03-23 12:17:21 +0100

    part of the purchase data

---
+++ src/Modules/Laser.Orchard.NwazetIntegration/Filters/InfoResultPaymentFilter.cs
@@ -0,0 +1,75 @@
+﻿using Laser.Orchard.NwazetIntegration.Models;
+using Laser.Orchard.NwazetIntegration.Services;
+using Laser.Orchard.NwazetIntegration.ViewModels;
+using Laser.Orchard.PaymentGateway.Controllers;
+using Laser.Orchard.PaymentGateway.ViewModels;
+using Nwazet.Commerce.Models;
+using Orchard.ContentManagement;
+using Orchard.Mvc;
+using Orchard.Mvc.Filters;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.NwazetIntegration.Filters {
+    public class InfoResultPaymentFilter : FilterProvider, IActionFilter {
+        private readonly IContentManager _contentManager;
+        private readonly IGTMProductService _GTMProductService;
+
+        public InfoResultPaymentFilter(
+            IContentManager contentManager,
+            IGTMProductService GTMProductService) {
+            _contentManager = contentManager;
+            _GTMProductService = GTMProductService;
+        }
+
+
+        public void OnActionExecuted(ActionExecutedContext filterContext) {
+            if (filterContext.Controller is PaymentController
+                && filterContext.ActionDescriptor.ActionName.Equals("Info")) {
+                var viewResult = filterContext.Result as ViewResult;
+                if (viewResult != null) {
+                    var model = viewResult.Model as PaymentVM;
+                    if (model != null) {
+                        // populate ViewModel to send at shape
+                        var purchaseVM = new GTMPurchaseVM();
+                        purchaseVM.ActionField = new GTMActionField {
+                            Id = model.Record.TransactionId
+                        };
+
+                        // select the contentitemid which is the id of the order
+                        var orderId = model.Record.ContentItemId;
+                        // select order
+                        var order = _contentManager.Get<OrderPart>(orderId);
+                        var checkoutItems = order.Items.ToList();
+                        var products = _contentManager
+                           .GetMany<IContent>(
+                               checkoutItems.Select(p => p.ProductId).Distinct(),
+                               VersionOptions.Latest,
+                               QueryHints.Empty)
+                            .ToList();
+
+                        var productList = new List<GTMProductVM>();
+                        foreach (var p in products) {
+                            // populate list of GTMProductVM 
+                            var part = p.As<GTMProductPart>();
+                            _GTMProductService.FillPart(part);
+                            var vm = new GTMProductVM(part);
+                            var checkoutItem = checkoutItems
+                                .Where(c => c.ProductId == p.Id)
+                                .FirstOrDefault();
+                            // add quantity
+                            vm.Quantity = checkoutItem == null ? 0 : checkoutItem.Quantity;
+                            productList.Add(vm);
+                        }
+                        purchaseVM.ProductList = productList;
+
+                    }
+                }
+
+            }
+        }
+        public void OnActionExecuting(ActionExecutingContext filterContext) {
+        }
+    }
+}
---
+++ src/Modules/Laser.Orchard.NwazetIntegration/ViewModels/GTMPurchaseVM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Laser.Orchard.NwazetIntegration.ViewModels {
+    public class GTMPurchaseVM {
+        public GTMActionField ActionField { get; set; }
+        public List<GTMProductVM> ProductList { get; set; }
+    }
+
+
+    public class GTMActionField {
+        public string Id { get; set; }
+        public string Affiliation { get; set; }
+        public decimal Revenue { get; set; }
+        public decimal Tax { get; set; }
+        public decimal Shipping { get; set; }
+        public string Coupon { get; set; }
+    }
+}
