59c825add87acce886ed838fec433e89ea916d96
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2020-05-08 15:47:27 +0200

    - AdvancedSettings

---
+++ src/Modules/Laser.Orchard.AdvancedSettings/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.AdvancedSettings.Controllers {
+    public class AdminController : Controller, IUpdateModel {
+        public ActionResult Index() {
+            return null;
+        }
+
+        public void AddModelError(string key, LocalizedString errorMessage) {
+            ModelState.AddModelError(key, errorMessage.ToString());
+        }
+
+        bool IUpdateModel.TryUpdateModel<TModel>(TModel model, string prefix, string[] includeProperties, string[] excludeProperties) {
+            return TryUpdateModel(model, prefix, includeProperties, excludeProperties);
+        }
+    }
+}
