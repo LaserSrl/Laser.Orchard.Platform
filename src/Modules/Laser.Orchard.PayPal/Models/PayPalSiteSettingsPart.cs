ca3539d93b001e2f8ee20346110127d5da626e08
elena.lampugnani <elena.lampugnani@laser-group.com>
2020-06-29 17:40:29 +0200

    first part or paypal setting

---
+++ src/Modules/Laser.Orchard.PayPal/Models/PayPalSiteSettingsPart.cs
@@ -0,0 +1,18 @@
+﻿using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.PayPal.Models {
+    public class PayPalSiteSettingsPart : ContentPart {
+        public string SecretId {
+            get { return this.Retrieve(x => x.SecretId); }
+            set { this.Store(x => x.SecretId, value); }
+        }
+        public string ClientId {
+            get { return this.Retrieve(x => x.ClientId); }
+            set { this.Store(x => x.ClientId, value); }
+        }
+    }
+}
