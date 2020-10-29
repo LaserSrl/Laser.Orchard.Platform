1bf30e827c263c4809f9b6808a745b4b45f9cbe0
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-10-29 17:42:30 +0100

    Clean up of code and added comments to better explain usage of RazorExecute Token

--- src/Modules/Laser.Orchard.StartupConfig/Tokens/RazorCodeExecutionToken.cs
+++ src/Modules/Laser.Orchard.StartupConfig/Tokens/RazorCodeExecutionToken.cs
@@ -21,6 +21,14 @@ namespace Laser.Orchard.StartupConfig.Tokens {
 
         public void Describe(DescribeContext context) {
             context.For("Shape")
+                // Usage:
+                // {Shape.RazorExecute:filename}
+                // don't append the ".cshtml" suffix.
+                // The implementation will inject a bunch of stuff in the executing Razor.
+                // The data from the EvaluateContex will be accessible as a Dictionary<string, object>
+                // For example, to access the ContentItem for which the token is being evaluated from
+                // within the razor:
+                // var content = (IContent)Model.Tokens["Content"]
                    .Token("RazorExecute:*", T("RazorExecute:<file name>"), T("Executes the razor code in the specified file."));
         }
 
@@ -29,11 +37,6 @@ namespace Laser.Orchard.StartupConfig.Tokens {
                 .Token(t => t.StartsWith("RazorExecute", StringComparison.OrdinalIgnoreCase) ? t.Substring(0, (t.IndexOf(".") > 0 ? t.IndexOf(".") : t.Length)) : null,
                     (fullToken, data) => { _fullTokenName = fullToken; return ExecuteRazorCode(context, fullToken); })
                 .Chain(_fullTokenName, "Text", d => ExecuteRazorCode(context, _fullTokenName));
-        }
-
-        private string ExecuteRazorCode(string fullToken) {
-            string fileName = fullToken.Substring("RazorExecute:".Length);
-            return _razorExecuteService.Execute(fileName + ".cshtml", _currentContentAccessor.CurrentContentItem);
         }
 
         private string ExecuteRazorCode(EvaluateContext context, string fullToken) {
