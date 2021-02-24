bd891435742de7b4cfc47b01cc3fd7eda428caef
Matteo Ronco <matteo.ronco@laser-group.com>
2021-02-24 17:07:16 +0100

    add i18next js

--- src/Modules/Laser.Orchard.jQueryPlugins/ResourceManifest.cs
+++ src/Modules/Laser.Orchard.jQueryPlugins/ResourceManifest.cs
@@ -99,6 +99,9 @@ namespace Laser.Orchard.jQueryPlugins {
             manifest.DefineScript("BootstrapItalia")
                 .SetUrl("../BootstrapItalia/js/bootstrap-italia.bundle.min.js");
 
+            manifest.DefineScript("I18Next")
+                .SetCdn("https://unpkg.com/i18next@19.9.0/dist/umd/i18next.min.js")
+                .SetDependencies("jQuery");
             manifest.DefineScript("PasswordStrength")
                 .SetCdn("https://cdn.jsdelivr.net/npm/pwstrength-bootstrap@3.0.9/dist/pwstrength-bootstrap.min.js")
                 .SetDependencies("jQuery");
