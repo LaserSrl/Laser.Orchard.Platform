ef9ad99de11590025007f63beecbe7ec6a48f6d4
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2021-04-07 18:17:47 +0200

    - ApplePay Merchant Domain Association File - Added Payments Category to Payment providers and gateway

---
+++ src/Modules/Laser.Orchard.StartupConfig/AppleEnvironment/ViewModels/MerchantDomainAssociationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Orchard.Environment.Extensions;
+
+namespace Laser.Orchard.StartupConfig.AppleEnvironment.ViewModels {
+    [OrchardFeature("Laser.Orchard.ApplePay.DomainAssociation")]
+    public class MerchantDomainAssociationViewModel {
+        public string Text { get; set; }
+        public bool Enable { get; set; }
+    }
+}
