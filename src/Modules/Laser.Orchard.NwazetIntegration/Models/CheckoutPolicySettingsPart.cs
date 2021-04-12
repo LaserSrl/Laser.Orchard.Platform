c2a685dffb4fd3235c0f32c1638b51fdb1aac793
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2021-04-12 10:03:37 +0200

    WIP required policies for checkout
    Removed deprecated AddressesController.Index action
    Started adding stuff to have required checkout policies, following a similar
    logic and structure as what's done for registration.

---
+++ src/Modules/Laser.Orchard.NwazetIntegration/Models/CheckoutPolicySettingsPart.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.NwazetIntegration.Models {
+    public class CheckoutPolicySettingsPart : ContentPart {
+
+        public string[] PolicyTextReferences {
+            get {
+                return (Retrieve<string>("PolicyTextReferences") ?? "")
+                    .Split(new[] { ',', ' ' },
+                    StringSplitOptions.RemoveEmptyEntries);
+            }
+            set { Store("PolicyTextReferences", String.Join(", ", value)); }
+        }
+    }
+}
