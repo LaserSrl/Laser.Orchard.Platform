16b1893d24093f7a9b73ad2a414d93c208fb81f4
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2021-06-10 12:50:10 +0200

    Given a ContentItem with corresponding configured DynamicButtonToWorkflows:  - When a task is scheduled, prevent saving the ContentItem from scheduling a    new copy of the task  - When more than one button is configured, scheduling a task from each now    leads to correct execution of all tasks. It used to be that the last task    to be scheduled would override all previous ones and be the only executed one.

--- src/Modules/Laser.Orchard.ButtonToWorkflows/Handlers/DynamicButtonToWorflowsScheduledTaskHandler.cs
+++ src/Modules/Laser.Orchard.ButtonToWorkflows/Handlers/DynamicButtonToWorflowsScheduledTaskHandler.cs
@@ -10,7 +10,10 @@ namespace Laser.Orchard.ButtonToWorkflows.Handlers {
     public class DynamicButtonToWorflowsScheduledTaskHandler : IScheduledTaskHandler {
 
         private readonly IWorkflowManager _workflowManager;
-        private const string TaskType = "Laser.Orchard.DynamicButtonToWorkflows.Task";
+        public static string BaseTaskType = "Laser.Orchard.DynamicButtonToWorkflows.Task";
+        public static string TaskType(string buttonName) {
+            return string.Format("{0}_{1}", BaseTaskType, buttonName);
+        }
 
         public ILogger Logger { get; set; }
 
@@ -21,21 +24,46 @@ namespace Laser.Orchard.ButtonToWorkflows.Handlers {
 
         public void Process(ScheduledTaskContext context) {
             try {
-                if (context.Task.TaskType != TaskType) return;
+                var btnName = ButtonName(context.Task.TaskType);
+                if (btnName == null) {
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(btnName)) {
+                    btnName = context.Task.ContentItem.As<DynamicButtonToWorkflowsPart>().ButtonName;
+                }
+                if (string.IsNullOrWhiteSpace(btnName)) {
+                    // failed to figure out a valid button name
+                    return;
+                }
 
                 _workflowManager.TriggerEvent("DynamicButtonEvent", context.Task.ContentItem, () => 
-                    new Dictionary<string, object> { { "ButtonName", context.Task.ContentItem.As<DynamicButtonToWorkflowsPart>().ButtonName }, { "Content", context.Task.ContentItem } });
-            }
-            catch (Exception ex) {
-                Logger.Error(ex, "Error in DynamicButtonToWorflowsScheduledTaskHandler. ContentItem: {0}, ScheduledUtc: {1:yyyy-MM-dd HH.mm.ss}", context.Task.ContentItem, context.Task.ScheduledUtc);
-            }
-            finally {
+                    new Dictionary<string, object> {
+                        { "ButtonName", btnName },
+                        { "Content", context.Task.ContentItem } });
+            } catch (Exception ex) {
+                Logger.Error(ex, 
+                    "Error in DynamicButtonToWorflowsScheduledTaskHandler. ContentItem: {0}, ScheduledUtc: {1:yyyy-MM-dd HH.mm.ss}", 
+                    context.Task.ContentItem, context.Task.ScheduledUtc);
+            } finally {
                 if (context.Task.ContentItem.Has<DynamicButtonToWorkflowsPart>()) {
                     context.Task.ContentItem.As<DynamicButtonToWorkflowsPart>().ButtonName = "";
                     context.Task.ContentItem.As<DynamicButtonToWorkflowsPart>().MessageToWrite = "";
-                    context.Task.ContentItem.As<DynamicButtonToWorkflowsPart>().ActionAsync = false;
                 }
             }
+        }
+
+        private string ButtonName(string taskType) {
+            if (!taskType.StartsWith(BaseTaskType)) {
+                // the task has nothing to do with DynamicButtonToWorkflow
+                return null;
+            }
+            if (taskType.Equals(BaseTaskType, StringComparison.OrdinalIgnoreCase)) {
+                // The task doesn't carry the name of the button in its TaskType.
+                // This used to be the normal case, but it would prevent correct
+                // execution of different scheduled tasks from the same ContentItem.
+                return string.Empty;
+            }
+            return taskType.Substring(BaseTaskType.Length + 1);
         }
     }
 }
