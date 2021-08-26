86bd0e278c6df4ddb55cda5c7db2551a1c524ae4
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2021-08-26 16:42:04 +0200

    Code refactoring and comments.

--- src/Modules/Laser.Orchard.NwazetIntegration/Services/FacebookShop/FacebookShopService.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Services/FacebookShop/FacebookShopService.cs
@@ -45,7 +45,7 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
         public bool CheckBusiness(FacebookShopServiceContext context) {
             string url = context.ApiBaseUrl + (context.ApiBaseUrl.EndsWith("/") ? context.BusinessId : "/" + context.BusinessId);
 
-            url = string.Format(url + "?access_token={0}", GenerateAccessToken(context));
+            url = string.Format(url + "?access_token={0}", context.AccessToken);
 
             HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
             try {
@@ -65,7 +65,7 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
         public bool CheckCatalog(FacebookShopServiceContext context) {
             string url = context.ApiBaseUrl + (context.ApiBaseUrl.EndsWith("/") ? context.CatalogId : "/" + context.CatalogId);
 
-            url = string.Format(url + "?access_token={0}", GenerateAccessToken(context));
+            url = string.Format(url + "?access_token={0}", context.AccessToken);
 
             HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
             try {
@@ -80,63 +80,6 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
                 return false;
             }
             return false;
-        }
-
-        public string GenerateAccessToken(FacebookShopServiceContext context) {
-            // System user token should be saved in FacebookShopSiteSettingsPart.
-            // It should have the required permissions to manage the catalog syncronization.
-            // This key has no expiration.
-            return context.AccessToken;
-
-            // Everything else is commented in case it's needed in the future.
-            // Access code generation has the following steps:
-            // 1. Authorize the client: https://graph.facebook.com/v11.0/oauth/authorize?client_id=1534966686844583&scope=business_management,catalog_management&redirect_uri=https://win2016dev.westeurope.cloudapp.azure.com/OrchardPiovanelliA/TestLite
-            // 2. The previous call redirects to: https://win2016dev.westeurope.cloudapp.azure.com/OrchardPiovanelliA/TestLite?code=AQCe-_0L4pQGdvCVWApdknt03o2WttfmaTKkpUL7CJZjOeXEXUm62D6rkVzGN7fLRJqn7eYAHQ5cn_c7gfOzJDE2DXJPxjyWbphMrW1_WwSCFS6i3SWuZABo7xV8-NNAyBCdKF85ZydTV7A9UdecM6Iydkelxs0WzUX3IxI49A9ViZv78octsZCBRjHo5z0w9OaXDQGY56okJ8NuFvtCYkeDuKTu37Su7p2Jp7ONRGRGFii0hKAIADtFGw98DtVvBHQ33EebAau5TT1cPoBl1IHaiT2oUwQAsNaaUDBMo8KX0IPFPLd_bvEceMpx8stXIp9vIQek32yiNFRXQ06pt6Qo#_=_
-            // We need to save the parameter "code" from the previous query string (for this, we need a proper Controller).
-            // 3. At this point, with the code, we can generate the access_token:
-            // https://graph.facebook.com/v11.0/oauth/access_token?client_id=1534966686844583&scope=business_management,catalog_management&redirect_uri=https://win2016dev.westeurope.cloudapp.azure.com/OrchardPiovanelliA/TestLite&code=AQAdwntGxdiyn2ixmKjhARWQqmsVtiGHx8vSM5VxOeJwYhTPB17kU1ZTRx-qQ81daYlcxGaNO8hRnEa56nRrQztmZ1YViHvIQoatfLJS3b_gk9uYMu8lieBXxzIm5Q72z0VCrSeJ1K0AArzF19PzRPksNx1xO9Mt_ZgSwLapZrf4cz8yLV82OShxSpeZH_SknR5YvXAgNrF9-26sJ63cfNmm6A4XpzwO60jS3yrp0VGNCGf1Bt9T1keRi5F5Mo1nfM9Eopxih01fXh6nlq2iKytnxIZJ6vapg0iK8NhD_WL2r3c8sPuk52gpVv3HH3bQkq17JFYetOrvHWoTiCFDRmRr%23_=_&client_secret=cebbdd99828772a89f9cf4ce93754cbf#_=_
-            // Response of the previous call is in the following format:
-            //{
-            //  "access_token": "EAAV0C0gchqcBADm8ZBOgHI3hGAaa90ApypIxjHBPVHIQt6m0Dw4U16HJdQvZCS6agaTJTRdOMGlAtNlrIn255ZCvZB8Nna2BmiH85zvXAdvwf7nZC9k4yPfafyXGUUW1LbMQ8136NOZClFJN1ovV4dQbWJ6mvlMt9NuRnGQNspu8FEJves6QXG",
-            //  "token_type": "bearer",
-            //  "expires_in": 5181913 // 60 days
-            //}
-
-            //string scope = "business_management,catalog_management";
-
-            //string redirect_uri = "https://win2016dev.westeurope.cloudapp.azure.com/OrchardPiovanelliA/TestLite";
-            //string url = string.Format(
-            //        "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
-            //        context.AppId, redirect_uri, scope);
-
-            //// SysAdmin token
-            //var token = "EAAV0C0gchqcBALZAxI0y2rfzvR1v7AxXYxZA6hb2XRPIURuJGGRP5rAYCdlXJEPQkNJG5WWdwf7e3A7rLLQTBM6GucBX4aazKZAZALFCbM93BN1tdValDcnGBJRQ4kcZBQil9zv4e82pKQ7yuRMOX9MH3OnZB4GTgr0ag8bVCKgsLqcT3RHUtl";
-
-            //return token;
-
-            //return "EAAV0C0gchqcBACLwPhZC28TcidhU31whL9X1rUuDNRsWeofh15mUcHSPvPcw4njk2cafq0UGXSdlPxXCgcYDtLb58chLt0vPEkMPpsEP7Nf56fOWIGea9J2EL3NEjetBcxSmwjESIA8XHMI6vkG9Dp2BcKjZBZBQ6VAa67J1B486QRb196732lyEdaEwfEZASZAi6CDoyX2gzxe9eQQfyG7dsNYeRt5pUirs14ZBls3Y3MNtlZAwOJW";
-
-            // Commented because app access token doesn't have enough permissions.
-            //string grant_type = "client_credentials";
-            //string url = string.Format(context.ApiBaseUrl + (context.ApiBaseUrl.EndsWith("/") ? "" : "/") + 
-            //    "oauth/access_token?client_id={0}&client_secret={1}&grant_type={2}&redirect_uri={3}",
-            //        context.AppId, context.AppSecret, grant_type, "");
-
-            //HttpWebRequest request = System.Net.WebRequest.Create(url) as HttpWebRequest;
-            //string access_token = "";
-            //try {
-            //    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse) {
-            //        StreamReader reader = new StreamReader(response.GetResponseStream());
-            //        string vals = reader.ReadToEnd();
-            //        var json = JObject.Parse(vals);
-            //        if ((json["access_token"]).Type != JTokenType.Null)
-            //            access_token = (json["access_token"] ?? "").ToString();
-            //    }
-            //} catch (Exception ex) {
-            //    return "EAAV0C0gchqcBAC9SYZCve2PQm1AB1ZAlIZBynwoKAUHKdpCR2ME9gWjUKcq3m4hCfbv8ZCSc2mAtbmYqEQroq9EPEF8raZBuSkLkMOBDEvtD8HOPwiZAShxuBAe5Xc9NzpZA514LJaBe0RzHA4L9U4ZAU7pPLKfNZB1VRr4autBdVPAXM6AEEU2GLDl7Rx60lttYkYC7XFP5wcjpZAwcy8zYHN663GdWvtKWpAbp6vRU8Y3ZAorCGvL7FrE";
-            //}
-
-            //return access_token;
         }
 
         public FacebookShopRequestContainer SyncProduct(ContentItem product) {
@@ -184,8 +127,22 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
             return null;
         }
 
-        private string EncapsulateUpdateProductJson(string jsonStr) {
-            return "{\"requests\":[" + jsonStr + "]}";
+        public FacebookShopRequestContainer SyncProduct(FacebookShopProductUpdateRequest context) {
+            FacebookShopRequestContainer requestContainer = new FacebookShopRequestContainer();
+            requestContainer.Requests.Add(context);
+
+            // Facebook Shop Site Settings: I need url, catalog id and access token.
+            _fsssp = _workContext.GetContext().CurrentSite.As<FacebookShopSiteSettingsPart>();
+            FacebookShopServiceContext ctx = new FacebookShopServiceContext() {
+                ApiBaseUrl = _fsssp.ApiBaseUrl,
+                BusinessId = _fsssp.BusinessId,
+                CatalogId = _fsssp.CatalogId,
+                AccessToken = _fsssp.AccessToken
+            };
+
+            FacebookShopProductBatch(requestContainer);
+
+            return requestContainer;
         }
 
         public FacebookShopRequestContainer RemoveProduct(ContentItem product) {
@@ -227,27 +184,9 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
                 CatalogId = _fsssp.CatalogId,
                 AccessToken = _fsssp.AccessToken
             };
-           
-            FacebookShopProductBatch(requestContainer);
-
-            return requestContainer;
-        }
-
-        public FacebookShopRequestContainer SyncProduct(FacebookShopProductUpdateRequest context) {
-            FacebookShopRequestContainer requestContainer = new FacebookShopRequestContainer();
-            requestContainer.Requests.Add(context);
-            
-            // Facebook Shop Site Settings: I need url, catalog id and access token.
-            _fsssp = _workContext.GetContext().CurrentSite.As<FacebookShopSiteSettingsPart>();
-            FacebookShopServiceContext ctx = new FacebookShopServiceContext() {
-                ApiBaseUrl = _fsssp.ApiBaseUrl,
-                BusinessId = _fsssp.BusinessId,
-                CatalogId = _fsssp.CatalogId,
-                AccessToken = _fsssp.AccessToken
-            };
 
             FacebookShopProductBatch(requestContainer);
-            
+
             return requestContainer;
         }
 
@@ -264,15 +203,16 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
             // Query on published products, to send them all in a single request to Facebook api.
             // Every call to Facebook api sends the update of 20 products.
             int step = 20;
+            // I look for the FacebookShopPart because I may have some products I don't need to synchronize on Facebook Shop (without the FacebookShopProductPart or with the SynchronizeFacebookShopFlag disabled).
             var query = _contentManager.Query<FacebookShopProductPart, FacebookShopProductPartRecord>(VersionOptions.Published)
                 .Where(fp => fp.SynchronizeFacebookShop == true);
 
             for (int count = 0; count < query.Count(); count += step) {
-                // I look for the FacebookShopPart because I may have some products I don't need to synchronize on Facebook Shop.
                 var facebookParts = _contentManager.Query<FacebookShopProductPart, FacebookShopProductPartRecord>(VersionOptions.Published)
                     .Where(fp => fp.SynchronizeFacebookShop == true)
                     .Slice(count, step);
 
+                // I build a container for each slice of the query results.
                 FacebookShopRequestContainer requestContainer = new FacebookShopRequestContainer();
 
                 foreach (var facebookPart in facebookParts) {
@@ -291,45 +231,13 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
             }
         }
 
-        private IFacebookShopRequest GetJsonContext(ContentItem product) {
-            var facebookPart = product.As<FacebookShopProductPart>();
-            var productPart = product.As<ProductPart>();
-            // Content Item must be a product with the FacebookShopProductPart.
-            if (productPart != null && facebookPart != null) {
-                var jsonTemplate = facebookPart.Settings.GetModel<FacebookShopProductPartSettings>().JsonForProductUpdate;
-                if (string.IsNullOrWhiteSpace(jsonTemplate)) {
-                    // Fallback to FacebookShopSiteSettingsPart
-                    jsonTemplate = _fsssp.DefaultJsonForProductUpdate;
-                }
-
-                // jsonTemplate typically begins with a double '{' and ends with a double '}' (to make tokens work).
-                // For this reason, before deserialization, I need to replace tokens and replace double parenthesis.
-                string productJson = _tokenizer.Replace(jsonTemplate, facebookPart.ContentItem);
-                productJson = productJson.Replace("{{", "{").Replace("}}", "}");
-
-                var jsonContext = FacebookShopProductUpdateRequest.From(productJson);
-
-                CheckCompliance(jsonContext, facebookPart.ContentItem);
-
-                if (jsonContext != null && !jsonContext.Valid) {
-                    Logger.Debug(jsonContext.Message.Text);
-                }
-
-                return jsonContext;
-            } else {
-                Logger.Debug(T("Invalid Product part or Facebook part.").Text);
-            }
-
-            return null;
-        }
-
         /// <summary>
         /// Calls Facebook Api batch endpoint.
         /// </summary>
         /// <param name="container"></param>
         private void FacebookShopProductBatch(FacebookShopRequestContainer container) {
             if (container.Requests.Count == 0) {
-                Logger.Debug(T("No product in request container.").Text);
+                Logger.Debug(T("No products in request container.").Text);
             } else {
                 var jsonBody = container.ToJson();
                 string url = _fsssp.ApiBaseUrl + (_fsssp.ApiBaseUrl.EndsWith("/") ? _fsssp.CatalogId : "/" + _fsssp.CatalogId);
@@ -366,6 +274,38 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
                     Logger.Debug(ex, T("Invalid Facebook api response. Product is not synchronized on Facebook Shop.").Text);
                 }
             }
+        }
+
+        private IFacebookShopRequest GetJsonContext(ContentItem product) {
+            var facebookPart = product.As<FacebookShopProductPart>();
+            var productPart = product.As<ProductPart>();
+            // Content Item must be a product with the FacebookShopProductPart.
+            if (productPart != null && facebookPart != null) {
+                var jsonTemplate = facebookPart.Settings.GetModel<FacebookShopProductPartSettings>().JsonForProductUpdate;
+                if (string.IsNullOrWhiteSpace(jsonTemplate)) {
+                    // Fallback to FacebookShopSiteSettingsPart
+                    jsonTemplate = _fsssp.DefaultJsonForProductUpdate;
+                }
+
+                // jsonTemplate typically begins with a double '{' and ends with a double '}' (to make tokens work).
+                // For this reason, before deserialization, I need to replace tokens and replace double parenthesis.
+                string productJson = _tokenizer.Replace(jsonTemplate, facebookPart.ContentItem);
+                productJson = productJson.Replace("{{", "{").Replace("}}", "}");
+
+                var jsonContext = FacebookShopProductUpdateRequest.From(productJson);
+
+                CheckCompliance(jsonContext, facebookPart.ContentItem);
+
+                if (jsonContext != null && !jsonContext.Valid) {
+                    Logger.Debug(jsonContext.Message.Text);
+                }
+
+                return jsonContext;
+            } else {
+                Logger.Debug(T("Invalid Product part or Facebook part.").Text);
+            }
+
+            return null;
         }
 
         /// <summary>
@@ -464,19 +404,20 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
                 // Url is empty, I need to create it from scratch.
                 UrlHelper urlHelper = new UrlHelper(_workContext.GetContext().HttpContext.Request.RequestContext);
                 // www.mysite.com/route-to-contentitem
-                // WARNING: if you're on localhost/apppool/tenant..., this doesn't work because it duplicates the app pool.
+                // WARNING: if you're on localhost/apppool/tenant..., this doesn't work because it duplicates the app pool for the following reason:
                 // BaseUrl is something like http://localhost/apppool/
                 // RouteUrl is something like /apppool/route-to-contentitem
                 context.Url = _workContext.GetContext().CurrentSite.BaseUrl + urlHelper.RouteUrl(product.ContentManager.GetItemMetadata(product).DisplayRouteValues);
             }
 
             if (!context.Url.StartsWith("http://", StringComparison.InvariantCultureIgnoreCase) && !context.Url.StartsWith("https://", StringComparison.InvariantCultureIgnoreCase)) {
-                // Url isn't a complete url (it doesn't start with http:// or https://).
+                // If url isn't a complete url (it doesn't start with http:// or https://).
                 // I need to add the BaseUrl.
                 context.Url = _workContext.GetContext().CurrentSite.BaseUrl + context.Url;
             }
 
             if (string.IsNullOrWhiteSpace(context.ImageUrl)) {
+                // TODO: check for the default image url.
                 context.Message = T("Invalid image url");
                 context.Valid = false;
                 return context;
@@ -484,6 +425,7 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
 
             if (string.IsNullOrWhiteSpace(context.Brand)) {
                 // TODO: what's the default brand of a product?
+                // For the moment, default brand of a product is assumed to be the SiteName.
                 context.Brand = _workContext.GetContext().CurrentSite.SiteName;
             }
 
--- src/Modules/Laser.Orchard.NwazetIntegration/Services/FacebookShop/IFacebookShopService.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Services/FacebookShop/IFacebookShopService.cs
@@ -5,7 +5,6 @@ namespace Laser.Orchard.NwazetIntegration.Services.FacebookShop {
     public interface IFacebookShopService : IDependency {
         bool CheckBusiness(FacebookShopServiceContext context);
         bool CheckCatalog(FacebookShopServiceContext context);
-        string GenerateAccessToken(FacebookShopServiceContext context);
         FacebookShopRequestContainer SyncProduct(ContentItem product);
         FacebookShopRequestContainer SyncProduct(FacebookShopProductUpdateRequest context);
         FacebookShopRequestContainer RemoveProduct(ContentItem product);
