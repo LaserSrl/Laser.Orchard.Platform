b747fcf816b67cd2571644640870f54b765e3e23
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2021-09-01 13:52:55 +0200

    Importing and Exporting FacebookShopProductPart.

--- src/Modules/Laser.Orchard.NwazetIntegration/Drivers/FacebookShopProductPartDriver.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Drivers/FacebookShopProductPartDriver.cs
@@ -2,12 +2,13 @@
 using Laser.Orchard.NwazetIntegration.ViewModels;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
 using Orchard.Environment.Extensions;
 using Orchard.Localization;
 
 namespace Laser.Orchard.NwazetIntegration.Drivers {
     [OrchardFeature("Laser.Orchard.FacebookShop")]
-    public class FacebookShopProductPartDriver :ContentPartDriver<FacebookShopProductPart>{
+    public class FacebookShopProductPartDriver : ContentPartDriver<FacebookShopProductPart> {
         protected override string Prefix => "FacebookShopProductPart";
 
         public FacebookShopProductPartDriver() {
@@ -35,6 +36,21 @@ namespace Laser.Orchard.NwazetIntegration.Drivers {
             }
 
             return Editor(part, shapeHelper);
+        }
+
+        protected override void Exporting(FacebookShopProductPart part, ExportContentContext context) {
+            context.Element(part.PartDefinition.Name)
+                .SetAttributeValue("SynchronizeFacebookShop", part.Record.SynchronizeFacebookShop);
+        }
+
+        protected override void Importing(FacebookShopProductPart part, ImportContentContext context) {
+            if (context.Data.Element(part.PartDefinition.Name) == null) {
+                return;
+            }
+
+            bool flag = false;
+            bool.TryParse(context.Attribute(part.PartDefinition.Name, "SynchronizeFacebookShop"), out flag);
+            part.SynchronizeFacebookShop = flag;
         }
     }
 }
