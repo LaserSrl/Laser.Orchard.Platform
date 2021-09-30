3331674325674fa7cd2daeb97f253faea46a0d64
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2021-09-30 12:23:50 +0200

    Modified queries to load containers in TranslatorTreeController faster.

---
+++ src/Modules/Laser.Orchard.Translator/Models/ContainerMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.Translator.Models {
+    /// <summary>
+    /// USE WITH CAUTION!
+    /// This class contains the message count for a container type - container name combination.
+    /// The count can be either the number of messages to translate, the number of translated messages or the total number of messages.
+    /// It depends on how the query used to get the messages is written.
+    /// </summary>
+    public class ContainerMessage {
+        public string ContainerType { get; set; }
+        public string ContainerName { get; set; }
+        public int Count { get; set; }
+    }
+}
