dbdee0b08bf9325561848313db27cb6947b99d60
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2022-01-18 11:13:37 +0100

    - Removed unused or broken resource definitions - Removed theadmin/styles/site.css suppression - reducred to 14px the default font size

--- src/Themes/KrakeAdmin/ResourceManifest.cs
+++ src/Themes/KrakeAdmin/ResourceManifest.cs
@@ -16,15 +16,7 @@ namespace KrakeAdmin {
             manifest.DefineStyle(BaseAdmin).SetUrl("baseadmin.min.css?v1.0", "baseadmin.css?v1.0");
             manifest.DefineStyle(KrakeAdmin).SetUrl("krakeadmin.min.css?v2.6", "krakeadmin.css?v2.6");
             manifest.DefineStyle(KrakeNavigation).SetUrl("krake-navicon.min.css?v1.4", "krake-navicon.css?v1.4");
-            //manifest.DefineStyle(Bootstrap).SetUrl("bootstrap.css", "bootstrap.css");
-            manifest.DefineStyle(Site).SetUrl("site.css", "site.css");
-
             manifest.DefineScript(TooltipImagezoom).SetUrl("TooltipsZoomimage.js", "TooltipsZoomimage.js");
-            //manifest.DefineScript(Bootstrap).SetUrl("bootstrap.js", "bootstrap.js");
-            //Style.Include("font-awesome.min.css").AtHead();
-            //Style.Include("iconic-font.min.css").AtHead();
-            //Style.Include("https://fonts.googleapis.com/css?family=Roboto:400,700,500,300,100,900").AtHead();
-
         }
     }
 }
