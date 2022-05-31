746f39ea00bdf74e0bc74e6db353c68df4916ad5
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2022-05-31 08:59:06 +0200

    OwnershipToken implementation and enhanement of OwnerEmail

--- src/Modules/Laser.Orchard.StartupConfig/Tokens/OwnerEmailToken.cs
+++ src/Modules/Laser.Orchard.StartupConfig/Tokens/OwnerEmailToken.cs
@@ -12,30 +12,44 @@ using Orchard.ContentManagement.Aspects;
 using Orchard.Security;
 
 namespace Laser.Orchard.StartupConfig.Tokens {
-    public class OwnerEmailToken : ITokenProvider {
+    public class OwnershipToken : ITokenProvider {
         private readonly IOrchardServices _orchardServices;
         public Localizer T { get; set; }
-        public OwnerEmailToken(IOrchardServices orchardServices) {
+        public OwnershipToken(IOrchardServices orchardServices) {
             T = NullLocalizer.Instance;
             _orchardServices = orchardServices;
         }
         public void Describe(DescribeContext context) {
-
-            context.For("Content", T("Owner email"), T("Owner email of content item "))
-                   .Token("OwnerEmail", T("OwnerEmail"), T("The email of user creator."))
-                   ;
+            context.For("Content", T("Content"), T("The processed content"))
+                   .Token("Ownership", T("Owner or User"), T("Owner of content item or User content item."))
+                   .Token("OwnerEmail", T("OwnerEmail"), T("The email of user creator."));
         }
 
         public void Evaluate(EvaluateContext context) {
-             context.For<IContent>("Content")
-                    .Token("OwnerEmail", x => GetTheValue(x));
+            context.For<IContent>("Content")
+                   .Token("OwnerEmail", x => GetTheValue(x))
+                   .Token("Ownership", x => GetTheContentOwnerOrUser(x))
+                   .Chain("User", "User", x => GetTheContentOwnerOrUser(x));
         }
-        private string GetTheValue(IContent part) {
-            IUser OwnerUser= (IUser)((dynamic)part.As<CommonPart>()).Owner;
-            if (OwnerUser != null)
-                return OwnerUser.Email;
-            else
-                return null;
+        private string GetTheValue(IContent content) {
+            if (content.As<CommonPart>() != null) {
+                return content.As<CommonPart>().Owner != null ? content.As<CommonPart>().Owner.Email : null;
+            }
+            else if (content.As<UserPart>() != null) {
+                return content.As<UserPart>().Email;
+            }
+            return null;
+
+        }
+
+        private ContentItem GetTheContentOwnerOrUser(IContent content) {
+            if (content.As<CommonPart>() != null) {
+                return content.As<CommonPart>().Owner != null ? content.As<CommonPart>().Owner.ContentItem : null;
+            }
+            else if (content.As<UserPart>() != null) {
+                return content.ContentItem;
+            }
+            return null;
         }
     }
-} 
+}
