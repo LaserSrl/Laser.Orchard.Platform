d37ab6fb885e98215e3ddf5a5dbc3e2459525f9f
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2022-06-29 12:19:13 +0200

    WIP work on provider for index shapes for pickup points

--- src/Modules/Laser.Orchard.NwazetIntegration/Services/PickupPointsCheckoutExtensionProvider.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Services/PickupPointsCheckoutExtensionProvider.cs
@@ -1,5 +1,7 @@
 ﻿using Laser.Orchard.NwazetIntegration.ViewModels;
+using Orchard.DisplayManagement;
 using Orchard.Environment.Extensions;
+using Orchard.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +11,29 @@ namespace Laser.Orchard.NwazetIntegration.Services {
     [OrchardFeature("Laser.Orchard.PickupPoints")]
     public class PickupPointsCheckoutExtensionProvider : BaseCheckoutExtensionProvider {
 
+        private readonly dynamic _shapeFactory;
+
+        public PickupPointsCheckoutExtensionProvider(
+            IShapeFactory shapeFactory)
+            : base() {
+
+            _shapeFactory = shapeFactory;
+
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
         private const string Prefix = "PickupPointsCheckout";
 
         public override IEnumerable<AdditionalIndexShippingAddressViewModel> 
             AdditionalIndexShippingAddressShapes() {
-            yield break;
+            yield return new AdditionalIndexShippingAddressViewModel {
+                TabTitle = T("Pickup Points").Text,
+                TabId = "tab-pickup-points",
+                NavId = "nav-pickup-points",
+                TabShape = _shapeFactory.CheckoutPickupPointsIndexShape()
+            };
         }
 
         public override void ProcessAdditionalIndexShippingAddressInformation(
