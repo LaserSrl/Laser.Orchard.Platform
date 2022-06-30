64f32b36b10379589d8ea05bbcb54b08557708ae
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2022-06-30 17:26:07 +0200

    wip validate selected pickup points

--- src/Modules/Laser.Orchard.NwazetIntegration/Models/PickupPointPart.cs
+++ src/Modules/Laser.Orchard.NwazetIntegration/Models/PickupPointPart.cs
@@ -58,5 +58,40 @@ namespace Laser.Orchard.NwazetIntegration.Models {
             get { return Retrieve(r => r.PostalCode); }
             set { Store(r => r.PostalCode, value); }
         }
+
+        public string AddressText(bool withCountry = true) {
+            var strElements = new List<string>();
+            if (!string.IsNullOrWhiteSpace(AddressLine1)) {
+                strElements.Add(AddressLine1);
+            }
+            if (!string.IsNullOrWhiteSpace(AddressLine2)) {
+                strElements.Add(AddressLine2);
+            }
+            if (!string.IsNullOrWhiteSpace(CityName)) {
+                strElements.Add(CityName);
+            }
+
+            if (!string.IsNullOrWhiteSpace(ProvinceName)) {
+                if (withCountry && !string.IsNullOrWhiteSpace(CountryName)) {
+                    strElements.Add($"{ProvinceName} ({CountryName})");
+                } else {
+                    strElements.Add(ProvinceName);
+                }
+            } else if (withCountry && !string.IsNullOrWhiteSpace(CountryName)) {
+                // edit the last element of the list so it has the country to it
+                // to prevent a comma separator where it wouldn't be required
+                var last = strElements.Last();
+                strElements.RemoveAt(strElements.Count - 1);
+                last = $"{last} ({CountryName})";
+                strElements.Add(last);
+            }
+
+
+            if (!string.IsNullOrWhiteSpace(PostalCode)) {
+                strElements.Add(PostalCode);
+            }
+
+            return string.Join(", ", strElements);
+        }
     }
 }
