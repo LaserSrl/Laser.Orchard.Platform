93d30db72f7535e4251770862c52af4bc2984126
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2022-10-20 18:47:46 +0200

    Create a per request cache of DisplayText Handler

--- src/Modules/Laser.Orchard.StartupConfig/Handlers/DisplayTextPartHandler.cs
+++ src/Modules/Laser.Orchard.StartupConfig/Handlers/DisplayTextPartHandler.cs
@@ -7,15 +7,20 @@ using System.Linq;
 namespace Laser.Orchard.StartupConfig.Handlers {
     public class DisplayTextPartHandler : ContentHandler {
         private readonly ITokenizer _tokenizer;
+        private Dictionary<int,string> _displayText;
         public DisplayTextPartHandler(ITokenizer tokenizer) {
             _tokenizer = tokenizer;
+            _displayText = new Dictionary<int, string>();
         }
         protected override void GetItemMetadata(GetContentItemMetadataContext context) {
             var part = context.ContentItem.Parts.FirstOrDefault(p => p.PartDefinition.Name == "DisplayTextPart");
-            if(part != null) {
+            if (part != null) {
                 var settings = part.Settings.GetModel<DisplayTextPartSettings>();
                 var tokens = new Dictionary<string, object> { { "Content", part.ContentItem } };
-                context.Metadata.DisplayText = _tokenizer.Replace(settings.DisplayText, tokens, new ReplaceOptions { Encoding = ReplaceOptions.NoEncode });
+                if (_displayText[part.ContentItem.Id] == null) {
+                    _displayText.Add(part.ContentItem.Id, _tokenizer.Replace(settings.DisplayText, tokens, new ReplaceOptions { Encoding = ReplaceOptions.NoEncode })) ;
+                }
+                context.Metadata.DisplayText = _displayText[part.ContentItem.Id];
             }
         }
     }
