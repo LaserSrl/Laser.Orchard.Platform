3e1d64bd43d4b89b5e094d36ed2595cd6512ae34
Matteo Piovanelli <matteo.piovanelli@laser-group.com>
2022-11-11 17:30:33 +0100

    Added part to configure widgets for remote snippet/content fetching.

---
+++ src/Modules/Laser.Orchard.TenantBridges/Migrations/RemoteTenantContentMigrations.cs
@@ -0,0 +1,30 @@
+﻿using Laser.Orchard.TenantBridges.Models;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.TenantBridges.Migrations {
+    public class RemoteTenantContentMigrations : DataMigrationImpl {
+        public int Create() {
+
+            var contentSnippetPartName = typeof(RemoteTenantContentSnippetWidgetPart).Name;
+            ContentDefinitionManager
+                .AlterPartDefinition(contentSnippetPartName, 
+                    part => part
+                        .Attachable()
+                        .WithDescription("Use this part to configure the content we want to fetch from a remote tenant."));
+
+            ContentDefinitionManager
+                .AlterTypeDefinition("RemoteContentSnippetWidget",
+                    cfg => cfg
+                        .AsWidgetWithIdentity()
+                        .WithPart(contentSnippetPartName));
+                
+            return 1;
+        }
+    }
+}
---
+++ src/Modules/Laser.Orchard.TenantBridges/Models/RemoteTenantContentSnippetWidgetPart.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.TenantBridges.Models {
+    /// <summary>
+    /// Use this part to configure the content we want to fetch from
+    /// a remote tenant
+    /// </summary>
+    public class RemoteTenantContentSnippetWidgetPart : ContentPart {
+
+        public string RemoteTenantBaseUrl {
+            get { return this.Retrieve(x => x.RemoteTenantBaseUrl); }
+            set { this.Store(x => x.RemoteTenantBaseUrl, value); }
+        }
+    }
+}
