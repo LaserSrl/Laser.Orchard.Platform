cf270a0530d389607a0b507e46b6a10388c18f1c
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2023-05-26 16:35:48 +0200

    - prevent adding twice plugins options of TinyMce - Manage the new Orchardlinks and HtmlSnippets TinyMce plugins

--- src/Modules/Laser.Orchard.StartupConfig/TinyMceEnhancement/TinyMceEnhancementService.cs
+++ src/Modules/Laser.Orchard.StartupConfig/TinyMceEnhancement/TinyMceEnhancementService.cs
@@ -1,4 +1,5 @@
 ﻿using Orchard;
+using Orchard.ContentManagement;
 using Orchard.Environment.Descriptor.Models;
 using System;
 using System.Collections.Generic;
@@ -13,9 +14,13 @@ namespace Laser.Orchard.StartupConfig.TinyMceEnhancement {
         string GetFrontendCorePluginsList();
     }
     public class TinyMceEnhancementService : ITinyMceEnhancementService {
+        private enum TinyMceContextOptions { FrontEnd, Admin };
         private readonly IOrchardServices _orchardServices;
+        private ShellDescriptor _shellDescriptor;
         public TinyMceEnhancementService(IOrchardServices orchardServices) {
             _orchardServices = orchardServices;
+            _shellDescriptor = _orchardServices.WorkContext.Resolve<ShellDescriptor>();
+
         }
         public string GetCorePluginsList() {
             var toolbarPlugins = GetMediaToolbarPlugins();
@@ -29,20 +34,58 @@ namespace Laser.Orchard.StartupConfig.TinyMceEnhancement {
             var toolbarPlugins = GetMediaToolbarPlugins();
             var plugins = Constants.BasePlugins + toolbarPlugins.Item2;
             return Constants.BasePartialInit + @",
-                    toolbar: """ + Constants.BaseLeftToolbar + toolbarPlugins.Item1 + Constants.BaseRightToolbar + @""",
-                    plugins: [""" + plugins + @"""]";
+                    toolbar: """ + ReorganizeToolbar(Constants.BaseLeftToolbar + toolbarPlugins.Item1 + Constants.BaseRightToolbar, TinyMceContextOptions.Admin) + @""",
+                    plugins: [""" + ReorganizePlugins(plugins, TinyMceContextOptions.Admin) + @"""]";
         }
         public string GetFrontendDefaultInitScript() {
             var toolbarPlugins = GetFrontendMediaToolbarPlugins();
             var plugins = Constants.FrontendBasePlugins + toolbarPlugins.Item2;
             return Constants.FrontendBasePartialInit + @",
-                    toolbar: """ + Constants.FrontendBaseLeftToolbar + toolbarPlugins.Item1 + Constants.FrontendBaseRightToolbar + @""",
-                    plugins: [""" + plugins + @"""]";
+                    toolbar: """ + ReorganizeToolbar(Constants.FrontendBaseLeftToolbar + toolbarPlugins.Item1 + Constants.FrontendBaseRightToolbar, TinyMceContextOptions.FrontEnd) + @""",
+                    plugins: [""" + ReorganizePlugins(plugins, TinyMceContextOptions.FrontEnd) + @"""]";
         }
+
+
+        /// <summary>
+        /// Adds the plugins that depend from orchard features
+        /// </summary>
+        /// <param name="currentPlugins">a string representing the current plugin list</param>
+        /// <param name="context">Admin or FrontEnd</param>
+        /// <returns>a string representing the new reorganized plugin list</returns>
+        private string ReorganizePlugins(string currentPlugins, TinyMceContextOptions context) {
+            var settings = _orchardServices.WorkContext.CurrentSite.As<TinyMceSiteSettingsPart>();
+            if (context == TinyMceContextOptions.Admin) {
+                var contenPickerEnabled = (_shellDescriptor.Features.Any(x => x.Name == "Orchard.ContentPicker") ? true : false);
+                var tokensHtmlFilterEnabled = (_shellDescriptor.Features.Any(x => x.Name == "Orchard.Tokens.HtmlFilter") ? true : false);
+                if (contenPickerEnabled && tokensHtmlFilterEnabled && !currentPlugins.Contains("orchardcontentlinks")) {
+                    currentPlugins += ", orchardcontentlinks";
+                }
+            }
+            return currentPlugins;
+        }
+
+        /// <summary>
+        /// Adds the toolbar buttons that depend from orchard features
+        /// </summary>
+        /// <param name="currentToolbar">a string representing the current toolbar buttons list</param>
+        /// <param name="context">Admin or FrontEnd</param>
+        /// <returns>a string representing the new reorganized toolbar buttons list</returns>
+        private string ReorganizeToolbar(string currentToolbar, TinyMceContextOptions context) {
+            if (context == TinyMceContextOptions.Admin) {
+
+                var contenPickerEnabled = (_shellDescriptor.Features.Any(x => x.Name == "Orchard.ContentPicker") ? true : false);
+                var tokensHtmlFilterEnabled = (_shellDescriptor.Features.Any(x => x.Name == "Orchard.Tokens.HtmlFilter") ? true : false);
+                if (contenPickerEnabled && tokensHtmlFilterEnabled && !currentToolbar.Contains("orchardlink")) {
+                    currentToolbar = currentToolbar.Replace("link ", "link orchardlink ");
+                }
+            }
+            return currentToolbar;
+
+        }
+
         private Tuple<string, string> GetMediaToolbarPlugins() {
-            var shellDescriptor = _orchardServices.WorkContext.Resolve<ShellDescriptor>();
-            var mediaPickerEnabled = shellDescriptor.Features.Any(x => x.Name == "Orchard.MediaPicker") ? true : false;
-            var mediaLibraryEnabled = shellDescriptor.Features.Any(x => x.Name == "Orchard.MediaLibrary") ? true : false;
+            var mediaPickerEnabled = _shellDescriptor.Features.Any(x => x.Name == "Orchard.MediaPicker") ? true : false;
+            var mediaLibraryEnabled = _shellDescriptor.Features.Any(x => x.Name == "Orchard.MediaLibrary") ? true : false;
             var mediaPlugins = "";
             var mediaToolbar = "";
             if (mediaPickerEnabled) {
@@ -56,8 +99,7 @@ namespace Laser.Orchard.StartupConfig.TinyMceEnhancement {
             return new Tuple<string, string>(mediaToolbar, mediaPlugins);
         }
 
-        private Tuple<string,string> GetFrontendMediaToolbarPlugins() {
-            var shellDescriptor = _orchardServices.WorkContext.Resolve<ShellDescriptor>();
+        private Tuple<string, string> GetFrontendMediaToolbarPlugins() {
             var mediaPickerEnabled = false;
             var mediaLibraryEnabled = false;
             var mediaPlugins = "";
