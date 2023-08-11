6ba24d2618ce06ba5e6c9e8458f38869d7479eea
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2023-08-11 15:30:39 +0200

    Multilevel menu for bootstrap 4.x

--- src/Themes/KrakeDefaultTheme/Scripts/krake.js
+++ src/Themes/KrakeDefaultTheme/Scripts/krake.js
@@ -1,19 +1,40 @@
-﻿$(document).ready(function () {
-    // Bootstrap multilevel menu
-    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
-        if (!$(this).next().hasClass('show')) {
-            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-        }
-        var $subMenu = $(this).next(".dropdown-menu");
-        $subMenu.toggleClass('show');
+﻿$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    //we prevent bootstrap.toggle() to be fired
+    e.preventDefault();
+    e.stopPropagation();
+    multiLevelMenu_Click(e, $(this), '.dropdown-menu');
+});
+$('a.nav-link.dropdown-toggle').on('click', function (e) {
+    multiLevelMenu_Click(e, $(this).parent().first(), '.dropdown-menu');
+});
 
-
-        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
-            $('.dropdown-submenu .show').removeClass("show");
-        });
-
-
-        return false;
+function multiLevelMenu_Click(event, sender, subsequentSelector) {
+    // sender is the clicked link
+    //we find all subsequent .dropdown-menu and we remove all the "show" cssclass
+    sender.find(subsequentSelector).each(function (index) {
+        $(this).parents(subsequentSelector).first().find('.show').removeClass('show');
+    });
+    //we find all sibling .dropdown-menu and we remove all the "show" cssclass
+    sender.next(subsequentSelector).each(function (index) {
+        $(this).parents(subsequentSelector).first().find('.show').removeClass('show');
     });
 
-});
+
+    // for the first subsequent .dropdownmenu we toggle the "show" cssclass
+    var $subMenu = sender.next(subsequentSelector);
+    $subMenu.toggleClass('show');
+
+    return false;
+    
+    if (!sender.next().hasClass('show')) {
+        sender.parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+    var $subMenu = sender.next('.dropdown-menu');
+    $subMenu.toggleClass('show');
+
+
+    sender.parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+        $('.dropdown-submenu .show').removeClass('show');
+    });
+    return false;
+}
