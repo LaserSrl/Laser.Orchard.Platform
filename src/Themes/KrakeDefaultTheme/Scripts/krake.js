d296679d58eac3d635c19839afa79f4b76ee08f7
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2023-08-22 10:33:09 +0200

    Fixes the multilevel menu - mobile and desktop views

--- src/Themes/KrakeDefaultTheme/Scripts/krake.js
+++ src/Themes/KrakeDefaultTheme/Scripts/krake.js
@@ -9,15 +9,21 @@ $('a.nav-link.dropdown-toggle').on('click', function (e) {
 });
 
 function multiLevelMenu_Click(event, sender, subsequentSelector) {
+    var senderParent = sender.parent();
+    if (sender.tagName == "A") {
+        
+    }
+
     // we find all subsequent .dropdown-menu and we remove all the "show" cssclass
     // so all the subsequent doprdown menus will be hidden
-    sender.find(subsequentSelector).each(function (index) {
-        $(this).parents(subsequentSelector).first().find('.show').removeClass('show');
+    senderParent.find(subsequentSelector).each(function (index) {
+        $(this).first().find('.show').removeClass('show');
+        //$(this).parents(subsequentSelector).first().find('.show').removeClass('show');
     });
     // we find all sibling .dropdown-menu and we remove all the "show" cssclass
     // so all the sibling doprdown menus will be hidden
-    sender.next(subsequentSelector).each(function (index) {
-        $(this).parents(subsequentSelector).first().find('.show').removeClass('show');
+    senderParent.siblings().each(function (index) {
+        $(this).first().find('.show').removeClass('show');
     });
 
 
