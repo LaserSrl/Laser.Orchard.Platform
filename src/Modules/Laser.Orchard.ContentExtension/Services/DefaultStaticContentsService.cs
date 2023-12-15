b94ea9220d3ab25e9a314c365cc38a546b5a2a5d
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2023-12-15 16:26:22 +0100

    Implements static files routes with a single route with constraint strategy

---
+++ src/Modules/Laser.Orchard.ContentExtension/Services/DefaultStaticContentsService.cs
@@ -0,0 +1,35 @@
+﻿using Orchard;
+using Orchard.Environment.Extensions;
+using Orchard.FileSystems.Media;
+using Orchard.Logging;
+using Orchard.Mvc.Routes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Laser.Orchard.ContentExtension.Services
+{
+    [OrchardFeature("Laser.Orchard.ContentExtension.StaticContents")]
+    public class DefaultStaticContentsService : IStaticContentsService
+    {
+        private readonly IStorageProvider _storageProvider;
+
+        public DefaultStaticContentsService(IStorageProvider storageProvider)
+        {
+            _storageProvider = storageProvider;
+        }
+
+        public string GetBaseFolder()
+        {
+            string staticContentsFolder = _storageProvider.GetPublicUrl("__StaticContents");
+            string folder = HostingEnvironment.MapPath(staticContentsFolder);
+            return folder;
+        }
+    }
+}
---
+++ src/Modules/Laser.Orchard.ContentExtension/Services/StaticContentsConstraint.cs
@@ -0,0 +1,39 @@
+﻿using Orchard;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Routing;
+
+namespace Laser.Orchard.ContentExtension.Services
+{
+
+    public interface IStaticContentsConstraint : IRouteConstraint, ISingletonDependency
+    {
+
+    }
+    public class StaticContentsConstraint : IStaticContentsConstraint
+    {
+        private readonly IStaticContentsService _staticContentsService;
+
+        public StaticContentsConstraint(IStaticContentsService staticContentsService)
+        {
+            _staticContentsService = staticContentsService;
+        }
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            var path = values["path"] ?? "";
+            if (string.IsNullOrWhiteSpace(path.ToString()))
+            {
+                return false;
+            }
+
+            string staticContentsFolder = _staticContentsService.GetBaseFolder();
+            string filePath = Path.Combine(staticContentsFolder, path.ToString().Replace("/", "\\"));
+            return System.IO.File.Exists(filePath);
+        }
+    }
+}
