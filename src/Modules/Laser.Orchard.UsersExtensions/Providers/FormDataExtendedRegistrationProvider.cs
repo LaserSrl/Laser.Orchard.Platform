ba1230aabd9710f9169a18946a528145004ecf7b
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2024-07-04 17:17:12 +0200

    Implemented FormDataExtendedRegistrationProvider for TextFields.

--- src/Modules/Laser.Orchard.UsersExtensions/Providers/FormDataExtendedRegistrationProvider.cs
+++ src/Modules/Laser.Orchard.UsersExtensions/Providers/FormDataExtendedRegistrationProvider.cs
@@ -1,11 +1,39 @@
 ﻿using Newtonsoft.Json.Linq;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Fields;
+using System;
+using System.Linq;
 using System.Web;
 
 namespace Laser.Orchard.UsersExtensions.Providers {
     public class FormDataExtendedRegistrationProvider : IExtendedRegistrationProvider {
-        public void FillCustomFields(HttpRequestBase request, JObject registeredData) {
-            if (request.Form !=  null && request.Form.Keys.Count > 0) {
+        private readonly IContentManager _contentManager;
 
+        public FormDataExtendedRegistrationProvider(IContentManager contentManager) {
+            _contentManager = contentManager;
+        }
+
+        public void FillCustomFields(HttpRequestBase request, JObject registeredData) {
+            if (request.Form != null && request.Form.Keys.Count > 0) {
+                var user = _contentManager.Get(GetUserIdFromRegisteredData(registeredData));
+                if (user != null) {
+                    var allFields = user.Parts.SelectMany(pa => pa.Fields);
+
+                    foreach (var key in request.Form.Keys) {
+                        var field = allFields.FirstOrDefault(fi => fi.PartFieldDefinition.Name.Equals(key.ToString(), StringComparison.OrdinalIgnoreCase));
+                        if (field != null) {
+                            switch (field.FieldDefinition.Name.ToLowerInvariant()) {
+                                case "textfield":
+                                    ((TextField)field).Value = request.Form[key.ToString()].ToString();
+                                    break;
+
+                                default:
+                                    //((TextField)field).Value = request.Form[key.ToString()].ToString();
+                                    break;
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -17,11 +45,12 @@ namespace Laser.Orchard.UsersExtensions.Providers {
             //  "UserId": 80,
             //  "ContactId": 78
             // },
-            var userId = 0;
+            var userId = registeredData["UserId"].ToString();
 
+            var intId = 0;
+            int.TryParse(userId, out intId);
 
-
-            return userId;
+            return intId;
         }
     }
 }
--- src/Modules/Laser.Orchard.UsersExtensions/Providers/RequestBodyExtendedRegistrationProvider.cs
+++ src/Modules/Laser.Orchard.UsersExtensions/Providers/RequestBodyExtendedRegistrationProvider.cs
@@ -1,11 +1,48 @@
 ﻿using Newtonsoft.Json.Linq;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Fields;
+using System;
+using System.IO;
+using System.Linq;
 using System.Web;
 
 namespace Laser.Orchard.UsersExtensions.Providers {
     public class RequestBodyExtendedRegistrationProvider : IExtendedRegistrationProvider {
+        private readonly IContentManager _contentManager;
+
+        public RequestBodyExtendedRegistrationProvider(IContentManager contentManager) {
+            _contentManager = contentManager;
+        }
+
         public void FillCustomFields(HttpRequestBase request, JObject registeredData) {
             if (request.InputStream.Length > 0) {
+                var user = _contentManager.Get(GetUserIdFromRegisteredData(registeredData));
+                if (user != null) {
+                    var allFields = user.Parts.SelectMany(pa => pa.Fields);
+                    using (var reader = new StreamReader(request.InputStream)) {
+                        // Force / reset the stream position
+                        request.InputStream.Position = 0;
+                        string respJson = reader.ReadToEnd();
+                        var json = JObject.Parse(respJson);
 
+                        for (int i = 0; i <= json.Count - 1; i++) {
+                            //var t = json[i];
+                            
+                        }
+
+                        //foreach (var t in json.ChildrenTokens) {
+                        //    var field = allFields.FirstOrDefault(fi => fi.PartFieldDefinition.Name.Equals(t.Name, StringComparison.OrdinalIgnoreCase));
+                        //    if (field != null) {
+                        //        switch (field.FieldDefinition.Name) {
+                        //            default:
+                        //                ((TextField)field).Value = t.Value;
+                        //                break;
+                        //        }
+                        //    }
+                        //}
+                    }
+
+                }
             }
         }
 
@@ -16,10 +53,12 @@ namespace Laser.Orchard.UsersExtensions.Providers {
             //  "UserId": 80,
             //  "ContactId": 78
             // },
-            var userId = 0;
+            var userId = registeredData["UserId"].ToString();
 
+            var intId = 0;
+            int.TryParse(userId, out intId);
 
-            return userId;
+            return intId;
         }
     }
 }
