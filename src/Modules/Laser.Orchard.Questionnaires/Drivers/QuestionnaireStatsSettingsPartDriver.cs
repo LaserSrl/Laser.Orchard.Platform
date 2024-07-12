63d44807b93d28c22b7567467f5722b6df8328d2
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2024-07-12 15:48:47 +0200

    Added feature Laser.Orchard.QuestionnaireStatsExport. Moved the questionnaire detail section to a separate shape for future use to generate pdf repors.

---
+++ src/Modules/Laser.Orchard.Questionnaires/Drivers/QuestionnaireStatsSettingsPartDriver.cs
@@ -0,0 +1,28 @@
+﻿using Laser.Orchard.Questionnaires.Helpers;
+using Laser.Orchard.Questionnaires.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+
+namespace Laser.Orchard.Questionnaires.Drivers {
+    [OrchardFeature("Laser.Orchard.QuestionnaireStatsExport")]
+    public class QuestionnaireStatsSettingsPartDriver : ContentPartDriver<QuestionnaireStatsSettingsPart> {
+        public const string TemplateName = "Parts/QuestionnaireStatsSettings";
+        
+        protected override string Prefix { get { return "QuestionnaireStatsSettingsPartDriver"; } }
+
+        protected override DriverResult Editor(QuestionnaireStatsSettingsPart part, dynamic shapeHelper) {
+            return ContentShape("Parts_QuestionnaireStatsSettings_Edit",
+                () => shapeHelper.EditorTemplate(
+                    TemplateName: TemplateName,
+                    Model: part,
+                    Prefix: Prefix))
+                .OnGroup(QuestionnaireHelper.SettingsGroupId);
+        }
+
+        protected override DriverResult Editor(QuestionnaireStatsSettingsPart part, IUpdateModel updater, dynamic shapeHelper) {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, shapeHelper);
+        }
+    }
+}
---
+++ src/Modules/Laser.Orchard.Questionnaires/Helpers/QuestionnaireHelper.cs
@@ -0,0 +1,8 @@
+﻿using Orchard.Localization;
+
+namespace Laser.Orchard.Questionnaires.Helpers {
+    public static class QuestionnaireHelper {
+        public static Localizer T = NullLocalizer.Instance;
+        public static string SettingsGroupId = T("Questionnaires").Text;
+    }
+}
