cc836b97faa4b033ac964ccf8698746c84ca3ffa
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2024-07-15 17:35:12 +0200

    Added dedicated controller for QuestionnaireStatsExport feature.

---
+++ src/Modules/Laser.Orchard.Questionnaires/Controllers/QuestionnaireStatsExportController.cs
@@ -0,0 +1,21 @@
+﻿using Laser.Orchard.Questionnaires.Services;
+using Laser.Orchard.Questionnaires.ViewModels;
+using Orchard.Environment.Extensions;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.Questionnaires.Controllers {
+        [OrchardFeature("Laser.Orchard.QuestionnaireStatsExport")]
+    [OutputCache(NoStore = true, Duration = 0)]
+    public class QuestionnaireStatsExportController : Controller {
+        private readonly IQuestionnairesServices _questionnairesServices;
+
+        public QuestionnaireStatsExportController(IQuestionnairesServices questionnairesServices) {
+            _questionnairesServices = questionnairesServices;
+        }
+
+        public ActionResult PdfExport(int questionnaireId, StatsDetailFilterContext filterContext, string redirectUrl) {
+            var model = _questionnairesServices.GetStats(questionnaireId, filterContext);
+            return Redirect(redirectUrl);
+        }
+    }
+}
