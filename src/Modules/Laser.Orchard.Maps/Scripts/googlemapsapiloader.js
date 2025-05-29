b7b088e6296dfdbf4d06cb8ecc762130aaac8b0a
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2025-05-29 14:19:53 +0200

    Forced Google Maps api version to 3.59

--- src/Modules/Laser.Orchard.Maps/Scripts/googlemapsapiloader.js
+++ src/Modules/Laser.Orchard.Maps/Scripts/googlemapsapiloader.js
@@ -2,7 +2,8 @@
     (g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({
         key: apiKey,
         language: lang,
-        libraries: libraries
+        libraries: libraries,
+        v: "3.59"
         // Add other bootstrap parameters as needed, using camel case.
         // Use the 'v' parameter to indicate the version to load (alpha, beta, weekly, etc.)
     });
